dde27dd2869d469c9480aa5b6379d35d68ea1020
gritsenko_s <gritsenko_s@999c22fd-c5eb-e245-b5be-d41808c6d2cd>
2013-02-28 01:31:00 +0000

    refactored properties access
    git-svn-id: svn://alcomputer/RunaWFE-4.x/trunk/projects/wfe@4463 999c22fd-c5eb-e245-b5be-d41808c6d2cd

--- wfe-alfresco/src/main/java/ru/runa/alfresco/search/Expr.java
+++ wfe-alfresco/src/main/java/ru/runa/alfresco/search/Expr.java
@@ -1,14 +1,27 @@
 package ru.runa.alfresco.search;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.regex.Matcher;
+
+import org.apache.commons.logging.LogFactory;
+
+import ru.runa.alfresco.AlfObject;
+import ru.runa.wfe.commons.CalendarUtil;
+
 /**
  * Represents search condition.
+ * 
  * @author dofs
  */
 public class Expr {
-    N operand;
-    Op operator;
-    Object[] params;
-    
+    private static final DateFormat DATE_QUERY_FORMAT = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.000Z");
+    private N operand;
+    private Op operator;
+    private Object[] params;
+
     public Expr() {
     }
 
@@ -22,11 +35,40 @@ public class Expr {
     }
 
     public Object[] getParams() {
-		return params;
-	}
-    
+        return params;
+    }
+
     @Override
     public String toString() {
-        return operator.toExpression(operand, params);
+        String result = operator.getRegexp();
+        result = result.replaceAll("\\$operand", Matcher.quoteReplacement(operand.toString()));
+        for (int i = 0; i < params.length; i++) {
+            String p;
+            if (params[i] != null) {
+                p = formatParam(params[i]);
+            } else {
+                p = "NULL";
+                LogFactory.getLog(getClass()).warn("Null param [" + i + "] in " + this);
+            }
+            result = result.replaceAll("\\$" + i, Matcher.quoteReplacement(p));
+        }
+        return result;
     }
+
+    private String formatParam(Object param) {
+        if (param == null) {
+            throw new NullPointerException(operator.getRegexp());
+        }
+        if (param instanceof Calendar) {
+            param = ((Calendar) param).getTime();
+        }
+        if (param instanceof Date) {
+            return CalendarUtil.format((Date) param, DATE_QUERY_FORMAT);
+        }
+        if (param instanceof AlfObject) {
+            return ((AlfObject) param).getUuidRef();
+        }
+        return param.toString();
+    }
+
 }
