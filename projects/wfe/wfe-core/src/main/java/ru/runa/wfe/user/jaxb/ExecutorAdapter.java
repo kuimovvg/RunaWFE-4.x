b9aca748eda4005de8d98636e468ae35905eaab4
Gritsenko_S <Gritsenko_S@999c22fd-c5eb-e245-b5be-d41808c6d2cd>
2013-03-07 19:32:08 +0000

    kerberos support tested
    git-svn-id: svn://alcomputer/RunaWFE-4.x/trunk@4502 999c22fd-c5eb-e245-b5be-d41808c6d2cd

--- projects/wfe/wfe-core/src/main/java/ru/runa/wfe/user/jaxb/ExecutorAdapter.java
+++ projects/wfe/wfe-core/src/main/java/ru/runa/wfe/user/jaxb/ExecutorAdapter.java
@@ -3,11 +3,14 @@ package ru.runa.wfe.user.jaxb;
 import javax.xml.bind.annotation.adapters.XmlAdapter;
 
 import org.apache.commons.beanutils.BeanUtils;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 
 import ru.runa.wfe.commons.ApplicationContextFactory;
 import ru.runa.wfe.user.Executor;
 
 public class ExecutorAdapter extends XmlAdapter<WfExecutor, Executor> {
+    private static final Log log = LogFactory.getLog(ExecutorAdapter.class);
 
     @Override
     public WfExecutor marshal(Executor executor) {
@@ -16,9 +19,8 @@ public class ExecutorAdapter extends XmlAdapter<WfExecutor, Executor> {
         wfExecutor.setExecutorClassName(executor.getClass().getName());
         try {
             BeanUtils.copyProperties(wfExecutor, executor);
-            // Map<String, String> map = BeanUtils.describe(executor);
         } catch (Exception e) {
-            e.printStackTrace();
+            log.error("ws conversion error", e);
         }
         return wfExecutor;
     }
