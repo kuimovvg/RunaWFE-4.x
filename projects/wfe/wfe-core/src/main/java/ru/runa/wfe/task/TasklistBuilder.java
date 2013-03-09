1dd2d1dfdf973af1259925989e4cad83562dd60a
gritsenko_s <gritsenko_s@999c22fd-c5eb-e245-b5be-d41808c6d2cd>
2013-03-09 11:52:49 +0000

    git-svn-id: svn://alcomputer/RunaWFE-4.x/trunk@4511 999c22fd-c5eb-e245-b5be-d41808c6d2cd

--- projects/wfe/wfe-core/src/main/java/ru/runa/wfe/task/TasklistBuilder.java
+++ projects/wfe/wfe-core/src/main/java/ru/runa/wfe/task/TasklistBuilder.java
@@ -26,12 +26,12 @@ import ru.runa.wfe.task.dto.WfTask;
 import ru.runa.wfe.task.dto.WfTaskFactory;
 import ru.runa.wfe.user.Actor;
 import ru.runa.wfe.user.Executor;
-import ru.runa.wfe.user.ExecutorDoesNotExistException;
 import ru.runa.wfe.user.Group;
 import ru.runa.wfe.user.dao.ExecutorDAO;
 
 import com.google.common.base.Objects;
 import com.google.common.collect.Lists;
+import com.google.common.collect.Sets;
 
 /**
  * Task list builder component.
@@ -58,33 +58,36 @@ public class TasklistBuilder {
         }
         int cacheVersion = taskCache.getCacheVersion();
         result = Lists.newArrayList();
-        Set<Executor> executorsToGetTasks = getActorsToGetTasks(actor, false);
-        Set<Executor> executorsToGetTasksSub = getSubstitutableExecutors(actor);
-        List<Task> tasks = new BatchPresentationHibernateCompiler(batchPresentation).getBatch(executorsToGetTasksSub, "executor", false);
+        Set<Executor> executorsToGetTasksByMembership = getExecutorsToGetTasks(actor, false);
+        Set<Executor> executorsToGetTasks = Sets.newHashSet(executorsToGetTasksByMembership);
+        for (Long substitutedActor : substitutionLogic.getSubstituted(actor)) {
+            executorsToGetTasks.addAll(getExecutorsToGetTasks(executorDAO.getActor(substitutedActor), true));
+        }
+        List<Task> tasks = new BatchPresentationHibernateCompiler(batchPresentation).getBatch(executorsToGetTasks, "executor", false);
         for (Task task : tasks) {
             try {
                 Executor taskExecutor = task.getExecutor();
                 ProcessDefinition processDefinition = processDefinitionLoader.getDefinition(task);
                 Interaction interaction = processDefinition.getInteractionNotNull(task.getNodeId());
                 String formType = (interaction == null ? null : interaction.getType());
-                if (executorsToGetTasks.contains(taskExecutor)) {
-                    // Our task - get.
+                if (executorsToGetTasksByMembership.contains(taskExecutor)) {
+                    // task acquired by membership rules
                     result.add(taskObjectFactory.create(task, actor, formType));
                     continue;
                 }
-                if (processDefinition.isSubsitutionIgnoredFor(task)) {
+                if (processDefinition.ignoreSubsitutionRulesForTask(task)) {
                     continue;
                 }
                 ExecutionContext executionContext = new ExecutionContext(processDefinition, task);
-                // Task to substitute (may be)
+                // Task (may be) acquired by substitution rules
                 if (taskExecutor instanceof Actor) {
-                    if (isCanSubToken(executionContext, task, (Actor) taskExecutor, actor)) {
+                    if (isTaskAcceptableBySubstitutionRules(executionContext, task, (Actor) taskExecutor, actor)) {
                         result.add(taskObjectFactory.create(task, (Actor) taskExecutor, formType));
                     }
                 } else {
-                    for (Actor actInGroup : executorDAO.getGroupActors((Group) taskExecutor)) {
-                        if (isCanSubToken(executionContext, task, actInGroup, actor)) {
-                            result.add(taskObjectFactory.create(task, actInGroup, formType));
+                    for (Actor groupActor : executorDAO.getGroupActors((Group) taskExecutor)) {
+                        if (isTaskAcceptableBySubstitutionRules(executionContext, task, groupActor, actor)) {
+                            result.add(taskObjectFactory.create(task, groupActor, formType));
                             break;
                         }
                     }
@@ -97,40 +100,30 @@ public class TasklistBuilder {
         return result;
     }
 
-    private Set<Executor> getActorsToGetTasks(Actor actor, boolean inactiveGroup) {
+    private Set<Executor> getExecutorsToGetTasks(Actor actor, boolean addOnlyInactiveGroups) {
         Set<Executor> executors = new HashSet<Executor>();
         executors.add(actor);
         Set<Group> upperGroups = executorDAO.getExecutorParentsAll(actor);
-        if (!inactiveGroup) {
-            executors.addAll(upperGroups);
-        } else {
-            for (Group gr : upperGroups) {
-                if (!hasActiveActorInGroup(gr)) {
-                    executors.add(gr);
+        if (addOnlyInactiveGroups) {
+            for (Group group : upperGroups) {
+                if (!hasActiveActorInGroup(group)) {
+                    executors.add(group);
                 }
             }
+        } else {
+            executors.addAll(upperGroups);
         }
         return executors;
     }
 
-    private Set<Executor> getSubstitutableExecutors(Actor actor) throws ExecutorDoesNotExistException {
-        Set<Executor> executorsToGetTasks = getActorsToGetTasks(actor, false);
-        Set<Executor> executorsToGetTasksSub = new HashSet<Executor>();
-        executorsToGetTasksSub.addAll(executorsToGetTasks);
-        for (Long substitutedActor : substitutionLogic.getSubstituted(actor)) {
-            executorsToGetTasksSub.addAll(getActorsToGetTasks(executorDAO.getActor(substitutedActor), true));
-        }
-        return executorsToGetTasksSub;
-    }
-
-    private boolean isCanSubToken(ExecutionContext executionContext, Task task, Actor asActor, Actor substitutorActor) {
-        TreeMap<Substitution, Set<Long>> mapOfSubstitionRule = substitutionLogic.getSubstitutors(asActor);
+    private boolean isTaskAcceptableBySubstitutionRules(ExecutionContext executionContext, Task task, Actor assignedActor, Actor substitutorActor) {
+        TreeMap<Substitution, Set<Long>> mapOfSubstitionRule = substitutionLogic.getSubstitutors(assignedActor);
         for (Map.Entry<Substitution, Set<Long>> substitutionRule : mapOfSubstitionRule.entrySet()) {
             Substitution substitution = substitutionRule.getKey();
             SubstitutionCriteria criteria = substitution.getCriteria();
             Set<Long> substitutors = substitutionRule.getValue();
             if (substitution instanceof TerminatorSubstitution) {
-                if (criteria == null || criteria.isSatisfied(executionContext, task, asActor, substitutorActor)) {
+                if (criteria == null || criteria.isSatisfied(executionContext, task, assignedActor, substitutorActor)) {
                     return false;
                 }
                 continue;
@@ -139,7 +132,7 @@ public class TasklistBuilder {
             boolean substitutionApplies = false;
             for (Long actorId : substitutors) {
                 Actor actor = executorDAO.getActor(actorId);
-                if (actor.isActive() && (criteria == null || criteria.isSatisfied(executionContext, task, asActor, actor))) {
+                if (actor.isActive() && (criteria == null || criteria.isSatisfied(executionContext, task, assignedActor, actor))) {
                     substitutionApplies = true;
                 }
                 if (Objects.equal(actor, substitutorActor)) {
