8c37bdfe126606bb50967ec074e6ebe9e000b295
gritsenko_s <gritsenko_s@999c22fd-c5eb-e245-b5be-d41808c6d2cd>
2013-03-18 15:44:42 +0000

    Variable.stringValue reduced to 900 symbols in order to create index in that field
    git-svn-id: svn://alcomputer/RunaWFE-4.x/trunk/projects/wfe@4550 999c22fd-c5eb-e245-b5be-d41808c6d2cd

--- wfe-core/src/main/java/ru/runa/wfe/commons/dbpatch/impl/ProcessListPerformancePatch.java
+++ wfe-core/src/main/java/ru/runa/wfe/commons/dbpatch/impl/ProcessListPerformancePatch.java
@@ -3,12 +3,14 @@ package ru.runa.wfe.commons.dbpatch.impl;
 import java.sql.Types;
 import java.util.List;
 
+import org.hibernate.SQLQuery;
 import org.hibernate.Session;
 
 import ru.runa.wfe.InternalApplicationException;
 import ru.runa.wfe.commons.DBType;
 import ru.runa.wfe.commons.dbpatch.DBPatch;
 import ru.runa.wfe.security.SecuredObjectType;
+import ru.runa.wfe.var.Variable;
 
 import com.google.common.collect.Lists;
 
@@ -28,8 +30,11 @@ public class ProcessListPerformancePatch extends DBPatch {
         sql.add(getDDLRemoveIndex("PERMISSION_MAPPING", "IX_PERMISSION_TYPE"));
         sql.add(getDDLCreateIndex("PERMISSION_MAPPING", "IX_PERMISSION_TYPE_ID", "TYPE_ID"));
         sql.add(getDDLRemoveColumn("PERMISSION_MAPPING", "TYPE"));
+        //
+        sql.add(getDDLModifyColumn("BPM_VARIABLE", "STRINGVALUE", dialect.getTypeName(Types.VARCHAR, Variable.MAX_STRING_SIZE, 0, 0)));
         sql.add(getDDLCreateIndex("BPM_VARIABLE", "IX_VARIABLE_NAME", "NAME"));
         sql.add(getDDLCreateIndex("BPM_VARIABLE", "IX_VARIABLE_VALUE", "STRINGVALUE"));
+        //
         String createIndex = "CREATE NONCLUSTERED INDEX IX_PERMISSION_INDEX ON PERMISSION_MAPPING (MASK,TYPE_ID,EXECUTOR_ID) INCLUDE (IDENTIFIABLE_ID)";
         if (dbType == DBType.MSSQL) {
             sql.add(createIndex);
@@ -49,6 +54,16 @@ public class ProcessListPerformancePatch extends DBPatch {
         for (SecuredObjectType type : SecuredObjectType.values()) {
             String q = "UPDATE PERMISSION_MAPPING SET TYPE_ID=" + type.ordinal() + " WHERE TYPE='" + type.name() + "'";
             log.info("Updated permission mappings (" + type + "): " + session.createSQLQuery(q).executeUpdate());
+        }
+        List<Object[]> list = session.createSQLQuery("SELECT ID, STRINGVALUE FROM BPM_VARIABLE WHERE LEN(STRINGVALUE) > " + Variable.MAX_STRING_SIZE)
+                .list();
+        for (Object[] objects : list) {
+            String string = (String) objects[1];
+            string = string.substring(0, Variable.MAX_STRING_SIZE);
+            SQLQuery query = session.createSQLQuery("UPDATE BPM_VARIABLE SET STRINGVALUE=:value WHERE ID=:id");
+            query.setParameter("id", objects[0]);
+            query.setParameter("value", string);
+            query.executeUpdate();
         }
     }
 
