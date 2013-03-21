f76baea99544a9b978ca36b4ba77b0e3a5c117e2
gritsenko_s <gritsenko_s@999c22fd-c5eb-e245-b5be-d41808c6d2cd>
2013-03-21 14:16:50 +0000

    timer should not complete task in history
    git-svn-id: svn://alcomputer/RunaWFE-4.x@4557 999c22fd-c5eb-e245-b5be-d41808c6d2cd

--- trunk/projects/wfe/wfe-core/src/main/java/ru/runa/wfe/commons/logic/InitializerLogic.java
+++ trunk/projects/wfe/wfe-core/src/main/java/ru/runa/wfe/commons/logic/InitializerLogic.java
@@ -41,7 +41,7 @@ import ru.runa.wfe.commons.dbpatch.DBPatch;
 import ru.runa.wfe.commons.dbpatch.UnsupportedPatch;
 import ru.runa.wfe.commons.dbpatch.impl.AddHierarchyProcess;
 import ru.runa.wfe.commons.dbpatch.impl.JbpmRefactoringPatch;
-import ru.runa.wfe.commons.dbpatch.impl.ProcessListPerformancePatch;
+import ru.runa.wfe.commons.dbpatch.impl.PerformancePatch;
 import ru.runa.wfe.commons.dbpatch.impl.TransitionLogPatch;
 import ru.runa.wfe.security.SecuredObjectType;
 import ru.runa.wfe.security.dao.PermissionDAO;
@@ -92,7 +92,7 @@ public class InitializerLogic {
         dbPatches.add(AddHierarchyProcess.class);
         dbPatches.add(JbpmRefactoringPatch.class); // 22
         dbPatches.add(TransitionLogPatch.class);
-        dbPatches.add(ProcessListPerformancePatch.class);
+        dbPatches.add(PerformancePatch.class);
     };
 
     @Autowired
