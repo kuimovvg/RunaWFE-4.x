c44dc00c25877a848d060d2930607f02083e91df
gritsenko_s <gritsenko_s@999c22fd-c5eb-e245-b5be-d41808c6d2cd>
2013-04-04 15:40:49 +0000

    js validation fix
    git-svn-id: svn://alcomputer/RunaWFE-4.x/trunk/projects/wfe@4613 999c22fd-c5eb-e245-b5be-d41808c6d2cd

--- wfe-core/src/main/java/ru/runa/wfe/validation/impl/StringLengthFieldValidator.java
+++ wfe-core/src/main/java/ru/runa/wfe/validation/impl/StringLengthFieldValidator.java
@@ -21,6 +21,19 @@ import ru.runa.wfe.validation.FieldValidator;
 
 public class StringLengthFieldValidator extends FieldValidator {
 
+    // also used in wfform-validate.ftl
+    public boolean getTrim() {
+        return getParameter(boolean.class, "doTrim", true);
+    }
+
+    public int getMinLength() {
+        return getParameter(int.class, "minLength", -1);
+    }
+
+    public int getMaxLength() {
+        return getParameter(int.class, "maxLength", -1);
+    }
+
     @Override
     public void validate() {
         String val = (String) getFieldValue();
@@ -28,15 +41,14 @@ public class StringLengthFieldValidator extends FieldValidator {
             // use a required validator for these
             return;
         }
-        boolean trim = getParameter(boolean.class, "doTrim", true);
-        if (trim) {
+        if (getTrim()) {
             val = val.trim();
             if (val.length() == 0) {
                 return;
             }
         }
-        int minLength = getParameter(int.class, "minLength", -1);
-        int maxLength = getParameter(int.class, "maxLength", -1);
+        int minLength = getMinLength();
+        int maxLength = getMaxLength();
         if ((minLength > -1) && (val.length() < minLength)) {
             addError();
         } else if ((maxLength > -1) && (val.length() > maxLength)) {
