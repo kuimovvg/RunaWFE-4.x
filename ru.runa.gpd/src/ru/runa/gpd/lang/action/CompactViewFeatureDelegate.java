a98759e36b00176c8fb70ea183e9d29cbd7724e0
gritsenko_s <gritsenko_s@999c22fd-c5eb-e245-b5be-d41808c6d2cd>
2013-05-27 15:17:37 +0000

    jpdl conjunction element created (https://sourceforge.net/p/runawfe/bugs/292/)
    git-svn-id: svn://alcomputer/RunaWFE-4.x/trunk/projects/gpd/plugins@4730 999c22fd-c5eb-e245-b5be-d41808c6d2cd

--- ru.runa.gpd/src/ru/runa/gpd/lang/action/CompactViewFeatureDelegate.java
+++ ru.runa.gpd/src/ru/runa/gpd/lang/action/CompactViewFeatureDelegate.java
@@ -3,6 +3,8 @@ package ru.runa.gpd.lang.action;
 import org.eclipse.jface.action.IAction;
 import org.eclipse.jface.viewers.ISelection;
 
+import ru.runa.gpd.lang.model.Conjunction;
+import ru.runa.gpd.lang.model.GraphElement;
 import ru.runa.gpd.lang.model.State;
 
 public class CompactViewFeatureDelegate extends BaseModelActionDelegate {
@@ -10,14 +12,24 @@ public class CompactViewFeatureDelegate extends BaseModelActionDelegate {
     public void selectionChanged(IAction action, ISelection selection) {
         super.selectionChanged(action, selection);
         if (action.isEnabled()) {
-            State state = getSelection();
-            action.setChecked(state.isMinimizedView());
+            GraphElement element = getSelection();
+            if (element instanceof State) {
+                action.setChecked(((State) element).isMinimizedView());
+            }
+            if (element instanceof Conjunction) {
+                action.setChecked(((Conjunction) element).isMinimizedView());
+            }
         }
     }
 
     @Override
     public void run(IAction action) {
-        State state = getSelection();
-        state.setMinimizedView(!state.isMinimizedView());
+        GraphElement element = getSelection();
+        if (element instanceof State) {
+            ((State) element).setMinimizedView(!((State) element).isMinimizedView());
+        }
+        if (element instanceof Conjunction) {
+            ((Conjunction) element).setMinimizedView(!((Conjunction) element).isMinimizedView());
+        }
     }
 }
