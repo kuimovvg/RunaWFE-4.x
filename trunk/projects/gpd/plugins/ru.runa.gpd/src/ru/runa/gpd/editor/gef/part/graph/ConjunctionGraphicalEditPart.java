958657503c6be636711d7aa4e1382263a958d1fd
gritsenko_s <gritsenko_s@999c22fd-c5eb-e245-b5be-d41808c6d2cd>
2013-05-28 18:33:52 +0000

    fix for minimazed states; bot task link validation
    git-svn-id: svn://alcomputer/RunaWFE-4.x@4732 999c22fd-c5eb-e245-b5be-d41808c6d2cd

--- trunk/projects/gpd/plugins/ru.runa.gpd/src/ru/runa/gpd/editor/gef/part/graph/ConjunctionGraphicalEditPart.java
+++ trunk/projects/gpd/plugins/ru.runa.gpd/src/ru/runa/gpd/editor/gef/part/graph/ConjunctionGraphicalEditPart.java
@@ -6,7 +6,7 @@ import ru.runa.gpd.editor.gef.figure.ConjunctionFigure;
 import ru.runa.gpd.lang.model.Conjunction;
 
 public class ConjunctionGraphicalEditPart extends LabeledNodeGraphicalEditPart {
-    
+
     @Override
     public Conjunction getModel() {
         return (Conjunction) super.getModel();
@@ -17,12 +17,6 @@ public class ConjunctionGraphicalEditPart extends LabeledNodeGraphicalEditPart {
         return (ConjunctionFigure) super.getFigure();
     }
 
-    @Override
-    protected void fillFigureResizePropertyNames(List<String> list) {
-        super.fillFigureResizePropertyNames(list);
-        list.add(PROPERTY_MINIMAZED_VIEW);
-    }
-    
     @Override
     protected void fillFigureUpdatePropertyNames(List<String> list) {
         super.fillFigureUpdatePropertyNames(list);
