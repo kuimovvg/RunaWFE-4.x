6097b71088df1266f0158083842614e9618ae42c
gritsenko_s <gritsenko_s@999c22fd-c5eb-e245-b5be-d41808c6d2cd>
2013-05-30 18:03:08 +0000

    fixed https://sourceforge.net/p/runawfe/bugs/233/
    git-svn-id: svn://alcomputer/RunaWFE-4.x/trunk/projects@4743 999c22fd-c5eb-e245-b5be-d41808c6d2cd

--- gpd/plugins/ru.runa.gpd/src/ru/runa/gpd/editor/CopyBuffer.java
+++ gpd/plugins/ru.runa.gpd/src/ru/runa/gpd/editor/CopyBuffer.java
@@ -1,14 +1,11 @@
-package ru.runa.gpd.editor.gef;
+package ru.runa.gpd.editor;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import org.eclipse.core.resources.IFolder;
 import org.eclipse.gef.ui.actions.Clipboard;
-import org.eclipse.jface.viewers.IStructuredSelection;
 
 import ru.runa.gpd.Localization;
-import ru.runa.gpd.editor.gef.part.graph.NodeGraphicalEditPart;
 import ru.runa.gpd.lang.model.Node;
 import ru.runa.gpd.lang.model.ProcessDefinition;
 
@@ -20,7 +17,7 @@ public class CopyBuffer {
 	
     private IFolder sourceFolder;
     private ProcessDefinition sourceDefinition;
-    private IStructuredSelection sourceSelection;
+    private List<Node> sourceNodes;
     
 	public CopyBuffer() {
         Object contents = Clipboard.getDefault().getContents();
@@ -29,23 +26,25 @@ public class CopyBuffer {
             if (array.length == 3) {
     	        sourceFolder = (IFolder) array[0];
     	        sourceDefinition = (ProcessDefinition) array[1];
-    	        sourceSelection = (IStructuredSelection) array[2];
+    	        sourceNodes = (List<Node>) array[2];
             }
         }
 	}
 
-    public List<Node> extractSourceNodes() {
-    	List<Node> result = new ArrayList<Node>();
-        for (Object object : sourceSelection.toList()) {
-            if (!(object instanceof NodeGraphicalEditPart)) {
-                continue;
-            }
-            Node node = ((NodeGraphicalEditPart) object).getModel();
-            result.add(node);
-        }
-    	return result;
+    public CopyBuffer(IFolder sourceFolder, ProcessDefinition sourceDefinition, List<Node> sourceNodes) {
+        this.sourceFolder = sourceFolder;
+        this.sourceDefinition = sourceDefinition;
+        this.sourceNodes = sourceNodes;
     }
-	
+
+    public void setToClipboard() {
+        Clipboard.getDefault().setContents(new Object[] { sourceFolder, sourceDefinition, sourceNodes });
+    }
+
+    public List<Node> getSourceNodes() {
+        return sourceNodes;
+    }
+    
 	public boolean isValid() {
 		return sourceFolder != null;
 	}
@@ -58,10 +57,6 @@ public class CopyBuffer {
 		return sourceDefinition;
 	}
 
-	public IStructuredSelection getSourceSelection() {
-		return sourceSelection;
-	}
-	
 	public static abstract class ExtraCopyAction {
         private final String groupName;
     	private final String name;
@@ -96,20 +91,6 @@ public class CopyBuffer {
 
 		public abstract void undo() throws Exception;
 
-		/*
-		@Override
-		public int hashCode() {
-		    int hash = groupName.hashCode();
-		    hash += 37*name.hashCode();
-		    return hash;
-		}
-		
-		@Override
-		public boolean equals(Object obj) {
-		    return hashCode() == obj.hashCode();
-		}
-		*/
-		
 		@Override
 		public String toString() {
 		    return getLabel();
