d216c826520b2118496b4180865df639af25020b
gritsenko_s <gritsenko_s@999c22fd-c5eb-e245-b5be-d41808c6d2cd>
2013-06-05 01:51:54 +0000

    actualization for 4.0.4
    git-svn-id: svn://alcomputer/RunaWFE-4.x/trunk@4750 999c22fd-c5eb-e245-b5be-d41808c6d2cd

--- projects/wfe/wfe-webservice-client/src/main/java/ru/runa/wfe/webservice/GetProcessTasksResponse.java
+++ projects/wfe/wfe-webservice-client/src/main/java/ru/runa/wfe/webservice/GetProcessTasksResponse.java
@@ -10,12 +10,12 @@ import javax.xml.bind.annotation.XmlType;
 
 
 /**
- * <p>Java class for getActiveTasksResponse complex type.
+ * <p>Java class for getProcessTasksResponse complex type.
  * 
  * <p>The following schema fragment specifies the expected content contained within this class.
  * 
  * <pre>
- * &lt;complexType name="getActiveTasksResponse">
+ * &lt;complexType name="getProcessTasksResponse">
  *   &lt;complexContent>
  *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
  *       &lt;sequence>
@@ -29,10 +29,10 @@ import javax.xml.bind.annotation.XmlType;
  * 
  */
 @XmlAccessorType(XmlAccessType.FIELD)
-@XmlType(name = "getActiveTasksResponse", propOrder = {
+@XmlType(name = "getProcessTasksResponse", propOrder = {
     "_return"
 })
-public class GetActiveTasksResponse {
+public class GetProcessTasksResponse {
 
     @XmlElement(name = "return")
     protected List<WfTask> _return;
--- projects/wfe/wfe-webservice-client/src/main/java/ru/runa/wfe/webservice/ProcessLogFilter.java
+++ projects/wfe/wfe-webservice-client/src/main/java/ru/runa/wfe/webservice/ProcessLogFilter.java
@@ -1,11 +1,8 @@
 
 package ru.runa.wfe.webservice;
 
-import java.util.ArrayList;
-import java.util.List;
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
-import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlType;
 
 
@@ -21,7 +18,6 @@ import javax.xml.bind.annotation.XmlType;
  *       &lt;sequence>
  *         &lt;element name="includeSubprocessLogs" type="{http://www.w3.org/2001/XMLSchema}boolean"/>
  *         &lt;element name="processId" type="{http://www.w3.org/2001/XMLSchema}long" minOccurs="0"/>
- *         &lt;element name="severities" type="{http://impl.service.wfe.runa.ru/}severity" maxOccurs="unbounded" minOccurs="0"/>
  *       &lt;/sequence>
  *     &lt;/restriction>
  *   &lt;/complexContent>
@@ -33,15 +29,12 @@ import javax.xml.bind.annotation.XmlType;
 @XmlAccessorType(XmlAccessType.FIELD)
 @XmlType(name = "processLogFilter", propOrder = {
     "includeSubprocessLogs",
-    "processId",
-    "severities"
+    "processId"
 })
 public class ProcessLogFilter {
 
     protected boolean includeSubprocessLogs;
     protected Long processId;
-    @XmlElement(nillable = true)
-    protected List<Severity> severities;
 
     /**
      * Gets the value of the includeSubprocessLogs property.
@@ -81,35 +74,6 @@ public class ProcessLogFilter {
      */
     public void setProcessId(Long value) {
         this.processId = value;
-    }
-
-    /**
-     * Gets the value of the severities property.
-     * 
-     * <p>
-     * This accessor method returns a reference to the live list,
-     * not a snapshot. Therefore any modification you make to the
-     * returned list will be present inside the JAXB object.
-     * This is why there is not a <CODE>set</CODE> method for the severities property.
-     * 
-     * <p>
-     * For example, to add a new item, do as follows:
-     * <pre>
-     *    getSeverities().add(newItem);
-     * </pre>
-     * 
-     * 
-     * <p>
-     * Objects of the following type(s) are allowed in the list
-     * {@link Severity }
-     * 
-     * 
-     */
-    public List<Severity> getSeverities() {
-        if (severities == null) {
-            severities = new ArrayList<Severity>();
-        }
-        return this.severities;
     }
 
 }
