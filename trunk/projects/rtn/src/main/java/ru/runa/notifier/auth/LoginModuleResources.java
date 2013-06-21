101e95d547bea4e8e200b8366dbbd87528a602a1
gritsenko_s <gritsenko_s@999c22fd-c5eb-e245-b5be-d41808c6d2cd>
2013-06-21 13:38:16 +0000

    updated build for v4.0.4
    git-svn-id: svn://alcomputer/RunaWFE-4.x@4822 999c22fd-c5eb-e245-b5be-d41808c6d2cd

--- trunk/projects/rtn/src/main/java/ru/runa/notifier/auth/LoginModuleResources.java
+++ trunk/projects/rtn/src/main/java/ru/runa/notifier/auth/LoginModuleResources.java
@@ -18,64 +18,36 @@
 
 package ru.runa.notifier.auth;
 
-import java.util.Enumeration;
-import java.util.HashMap;
 import java.util.Map;
 
-import ru.runa.wfe.commons.ResourceCommons;
+import ru.runa.wfe.commons.PropertyResources;
 
 /**
  * Created on 2006
  * 
  * @author Gritsenko_S
  */
-class LoginModuleResources extends ResourceCommons {
-
-    private final static String PROPERTY_FILE = "kerberos_module";
-    private static Map<String, String> loginModuleParams;
-    private static boolean inited = false;
+class LoginModuleResources {
+    private final static PropertyResources PROPERTIES = new PropertyResources("kerberos.properties");
 
     private static final String APP_NAME_PROPERTY = "appName";
     private static final String MODULE_CLASS_NAME_PROPERTY = "moduleClassName";
     private static final String SERVER_PRINCIPAL_PROPERTY = "serverPrincipal";
 
-    private LoginModuleResources() {
-        super(PROPERTY_FILE);
-    }
-
-    private static void init() {
-        if (!inited) {
-            loginModuleParams = new HashMap<String, String>();
-            Enumeration<String> propertiesEnum = getKeys(PROPERTY_FILE);
-            while (propertiesEnum.hasMoreElements()) {
-                String propertyName = propertiesEnum.nextElement();
-                String propertyValue = readProperty(propertyName, PROPERTY_FILE);
-                loginModuleParams.put(propertyName, propertyValue);
-            }
-            inited = true;
-        }
-    }
-
     public static Map<String, String> getInitParameters() {
-        init();
-        return loginModuleParams;
-    }
-
-    private static String getParameter(String name) {
-        init();
-        return loginModuleParams.get(name);
+        return PROPERTIES.getAllProperties();
     }
 
     public static String getAppName() {
-        return getParameter(APP_NAME_PROPERTY);
+        return PROPERTIES.getStringProperty(APP_NAME_PROPERTY);
     }
 
     public static String getServerPrincipal() {
-        return getParameter(SERVER_PRINCIPAL_PROPERTY);
+        return PROPERTIES.getStringProperty(SERVER_PRINCIPAL_PROPERTY);
     }
 
     public static String getModuleClassName() {
-        return getParameter(MODULE_CLASS_NAME_PROPERTY);
+        return PROPERTIES.getStringProperty(MODULE_CLASS_NAME_PROPERTY);
     }
 
 }
