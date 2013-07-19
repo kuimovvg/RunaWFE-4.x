0552b230b180773d3aafb338b531929760f74726
gritsenko_s <gritsenko_s@999c22fd-c5eb-e245-b5be-d41808c6d2cd>
2013-07-19 16:36:16 +0000

    bugfix: format and parse are reversed operations
    git-svn-id: svn://alcomputer/RunaWFE-4.x/trunk/projects@4919 999c22fd-c5eb-e245-b5be-d41808c6d2cd

--- wfe/wfe-core/src/main/java/ru/runa/wfe/var/format/ListFormat.java
+++ wfe/wfe-core/src/main/java/ru/runa/wfe/var/format/ListFormat.java
@@ -3,11 +3,15 @@ package ru.runa.wfe.var.format;
 import java.util.ArrayList;
 import java.util.List;
 
+import ru.runa.wfe.commons.TypeConversionUtil;
 import ru.runa.wfe.commons.web.WebHelper;
 import ru.runa.wfe.user.User;
 
 public class ListFormat implements VariableFormat<List<?>>, VariableDisplaySupport<List<?>>, VariableFormatContainer {
-    private String[] componentClassNames;
+    private static final String LIST_DELIMITER = ", ";
+    private static final String LIST_END = "]";
+    private static final String LIST_START = "[";
+    private String componentClassName;
 
     @Override
     public Class<?> getJavaClass() {
@@ -16,17 +20,21 @@ public class ListFormat implements VariableFormat<List<?>>, VariableDisplaySuppo
 
     @Override
     public void setComponentClassNames(String[] componentClassNames) {
-        if (componentClassNames.length == 1) {
-            this.componentClassNames = componentClassNames;
+        if (componentClassNames.length == 1 && componentClassNames[0] != null) {
+            this.componentClassName = componentClassNames[0];
         } else {
-            this.componentClassNames = new String[] { StringFormat.class.getName() };
+            this.componentClassName = StringFormat.class.getName();
         }
     }
 
     @Override
     public List<?> parse(String[] source) throws Exception {
+        if (source != null && source.length == 1 && source[0].startsWith(LIST_START) && source[0].endsWith(LIST_END)) {
+            String s = source[0].substring(LIST_START.length(), source[0].length() - LIST_END.length());
+            source = s.split(LIST_DELIMITER, -1);
+        }
         ArrayList list = new ArrayList(source.length);
-        VariableFormat<?> componentFormat = FormatCommons.create(componentClassNames[0]);
+        VariableFormat<?> componentFormat = FormatCommons.create(componentClassName);
         for (String string : source) {
             list.add(componentFormat.parse(new String[] { string }));
         }
@@ -34,23 +42,36 @@ public class ListFormat implements VariableFormat<List<?>>, VariableDisplaySuppo
     }
 
     @Override
-    public String format(List<?> obj) {
-        return obj.toString();
+    public String format(List<?> list) {
+        VariableFormat<Object> componentFormat = FormatCommons.create(componentClassName);
+        StringBuffer text = new StringBuffer();
+        text.append(LIST_START);
+        for (int i = 0; i < list.size(); i++) {
+            if (i != 0) {
+                text.append(LIST_DELIMITER);
+            }
+            Object object = list.get(i);
+            object = TypeConversionUtil.convertTo(componentFormat.getJavaClass(), object);
+            String value = componentFormat.format(object);
+            text.append(value);
+        }
+        text.append(LIST_END);
+        return text.toString();
     }
 
     @Override
     public String getHtml(User user, WebHelper webHelper, Long processId, String name, List<?> list) {
         StringBuffer html = new StringBuffer();
-        html.append("[");
+        html.append(LIST_START);
         for (int i = 0; i < list.size(); i++) {
             if (i != 0) {
-                html.append(", ");
+                html.append(LIST_DELIMITER);
             }
             Object object = list.get(i);
             String value = FormatCommons.getVarOut(user, object, webHelper, processId, name, i, null);
             html.append(value);
         }
-        html.append("]");
+        html.append(LIST_END);
         return html.toString();
     }
 
