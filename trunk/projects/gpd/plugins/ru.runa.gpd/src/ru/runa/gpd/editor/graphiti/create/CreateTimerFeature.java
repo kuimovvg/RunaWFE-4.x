45f8072fdd9b40f31e11fca7c5e4ded9529b244a
gritsenko_s <gritsenko_s@999c22fd-c5eb-e245-b5be-d41808c6d2cd>
2013-08-03 15:50:35 +0000

    timer can be placed on swimlane
    git-svn-id: svn://alcomputer/RunaWFE-4.x@4976 999c22fd-c5eb-e245-b5be-d41808c6d2cd

--- trunk/projects/gpd/plugins/ru.runa.gpd/src/ru/runa/gpd/editor/graphiti/create/CreateTimerFeature.java
+++ trunk/projects/gpd/plugins/ru.runa.gpd/src/ru/runa/gpd/editor/graphiti/create/CreateTimerFeature.java
@@ -4,11 +4,12 @@ import org.eclipse.graphiti.features.context.ICreateContext;
 
 import ru.runa.gpd.lang.model.ITimed;
 import ru.runa.gpd.lang.model.ProcessDefinition;
+import ru.runa.gpd.lang.model.Swimlane;
 
 public class CreateTimerFeature extends CreateElementFeature {
     @Override
     public boolean canCreate(ICreateContext context) {
         Object parentObject = getBusinessObjectForPictogramElement(context.getTargetContainer());
-        return (parentObject instanceof ProcessDefinition || parentObject instanceof ITimed);
+        return (parentObject instanceof ProcessDefinition || parentObject instanceof Swimlane || parentObject instanceof ITimed);
     }
 }
