9a3b8a6bb75e710adcef4037abf76558c18e9ff5
gritsenko_s <gritsenko_s@999c22fd-c5eb-e245-b5be-d41808c6d2cd>
2013-08-03 18:43:33 +0000

    externalized ru strings
    git-svn-id: svn://alcomputer/RunaWFE-4.x/trunk/projects/gpd/plugins@4981 999c22fd-c5eb-e245-b5be-d41808c6d2cd

--- ru.runa.gpd/src/ru/runa/gpd/util/PhraseDecliner_ru.java
+++ ru.runa.gpd/src/ru/runa/gpd/util/PhraseDecliner_ru.java
@@ -1,21 +1,28 @@
 package ru.runa.gpd.util;
 
+import org.eclipse.osgi.util.NLS;
+
+import ru.runa.gpd.PluginLogger;
+
 public class PhraseDecliner_ru extends PhraseDecliner {
- 
     @Override
-    public String declineDuration(String number, String base) {
-        Rule rule = Rule.find(base);
+    public String declineDuration(String delay, String unit) {
+        Rule rule = Rule.find(unit);
+        if (rule == null) {
+            PluginLogger.logInfo("ERROR: No rule found for '" + unit + "'");
+            return super.declineDuration(delay, unit);
+        }
         int pos;
-        if (number.endsWith("11") || number.endsWith("12") || number.endsWith("13") || number.endsWith("14")) {
+        if (delay.endsWith("11") || delay.endsWith("12") || delay.endsWith("13") || delay.endsWith("14")) {
             pos = 2;
-        } else if (number.endsWith("2") || number.endsWith("3") || number.endsWith("4")) {
+        } else if (delay.endsWith("2") || delay.endsWith("3") || delay.endsWith("4")) {
             pos = 1;
-        } else if (number.endsWith("1")) {
+        } else if (delay.endsWith("1")) {
             pos = 0;
         } else {
             pos = 2;
         }
-        return number + " " + applyRule(base, rule, pos);
+        return delay + " " + applyRule(unit, rule, pos);
     }
 
     private String applyRule(String base, Rule rule, int pos) {
@@ -34,12 +41,43 @@ public class PhraseDecliner_ru extends PhraseDecliner {
         return result;
     }
 
-    private enum Rule {
-        MIN(new int[] { 2, 1, 0, 0, 1, 1 }, new String[] { "ая", "а", "", "", "х", "" }), W(new int[] { 2, 1, 0, 0, 1, 1 }, new String[] { "ая", "я",
-                "", "", "х", "ь" }), D(new int[] { 1, 2, 1, 1, 1, 1 }, new String[] { "й", "ень", "х", "я", "х", "ей" }), H(new int[] { 1, 1, 1, 1,
-                1, 1 }, new String[] { "й", "", "х", "а", "х", "ов" }), Y(new int[] { 1, 1, 1, 1, 1, 4 }, new String[] { "й", "", "х", "а", "х",
-                "лет" }), MONTH(new int[] { 1, 1, 1, 1, 1, 1 }, new String[] { "й", "", "х", "а", "х", "ев" });
+    public static class Messages extends NLS {
+        private static final String BUNDLE_NAME = "ru.runa.gpd.util.PhraseDecliner";
+        public static String years;
+        public static String months;
+        public static String weeks;
+        public static String days;
+        public static String hours;
+        public static String minutes;
+        public static String seconds;
+        public static String a;
+        public static String x;
+        public static String ya;
+        public static String aya;
+        public static String y;
+        public static String ey;
+        public static String ov;
+        public static String let;
+        public static String en;
+        public static String ev;
+        public static String soft;
+        static {
+            NLS.initializeMessages(BUNDLE_NAME, PhraseDecliner_ru.Messages.class);
+        }
+    }
 
+    private static enum Rule {
+        MIN(new int[] { 2, 1, 0, 0, 1, 1 }, new String[] { Messages.aya, Messages.a, "", "", Messages.x, "" }),
+        //
+        W(new int[] { 2, 1, 0, 0, 1, 1 }, new String[] { Messages.aya, Messages.ya, "", "", Messages.x, Messages.soft }),
+        //
+        D(new int[] { 1, 2, 1, 1, 1, 1 }, new String[] { Messages.y, Messages.en, Messages.x, Messages.ya, Messages.x, Messages.ey }),
+        //
+        H(new int[] { 1, 1, 1, 1, 1, 1 }, new String[] { Messages.y, "", Messages.x, Messages.a, Messages.x, Messages.ov }),
+        //
+        Y(new int[] { 1, 1, 1, 1, 1, 4 }, new String[] { Messages.y, "", Messages.x, Messages.a, Messages.x, Messages.let }),
+        //
+        MONTH(new int[] { 1, 1, 1, 1, 1, 1 }, new String[] { Messages.y, "", Messages.x, Messages.a, Messages.x, Messages.ev });
         private final int[] charsToRemove;
         private final String[] charsToAdd;
 
@@ -49,22 +87,22 @@ public class PhraseDecliner_ru extends PhraseDecliner {
         }
 
         public static Rule find(String base) {
-            if (base.contains("часы")) {
+            if (base.contains(Messages.hours)) {
                 return H;
-            } else if (base.contains("минуты")) {
+            } else if (base.contains(Messages.minutes)) {
                 return MIN;
-            } else if (base.contains("дни")) {
+            } else if (base.contains(Messages.days)) {
                 return D;
-            } else if (base.contains("недели")) {
+            } else if (base.contains(Messages.weeks)) {
                 return W;
-            } else if (base.contains("месяцы")) {
+            } else if (base.contains(Messages.months)) {
                 return MONTH;
-            } else if (base.contains("года")) {
+            } else if (base.contains(Messages.years)) {
                 return Y;
-            } else if (base.contains("секунды")) {
+            } else if (base.contains(Messages.seconds)) {
                 return MIN;
             } else {
-                throw new IllegalArgumentException(base);
+                return null;
             }
         }
     }
