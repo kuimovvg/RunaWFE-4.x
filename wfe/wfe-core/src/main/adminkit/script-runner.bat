b5563dfbf2bff5a3a8140ab5d9dd1e39155308c5
gritsenko_s <gritsenko_s@999c22fd-c5eb-e245-b5be-d41808c6d2cd>
2013-08-04 04:42:12 +0000

    guava version updated to 14.0.1
    git-svn-id: svn://alcomputer/RunaWFE-4.x/trunk/projects@4984 999c22fd-c5eb-e245-b5be-d41808c6d2cd

--- wfe/wfe-core/src/main/adminkit/script-runner.bat
+++ wfe/wfe-core/src/main/adminkit/script-runner.bat
@@ -10,7 +10,7 @@ set PASSWORD="wf"
 REM ########End of Configuration###############
 
 
-set CLASSPATH=".\conf;.\lib\wfe-service.jar;.\lib\wfe-core.jar;.\lib\jbossall-client.jar;.\lib\commons-logging-1.1.0.jar;.\lib\commons-io-1.2.jar;.\lib\guava-12.0.jar"
+set CLASSPATH=".\conf;.\lib\wfe-service.jar;.\lib\wfe-core.jar;.\lib\jbossall-client.jar;.\lib\commons-logging-1.1.0.jar;.\lib\commons-io-1.2.jar;.\lib\guava-14.0.1.jar"
 
 java -cp %CLASSPATH% ru.runa.wfe.service.client.AdminScriptClient %SCRIPT_PATH% %LOGIN% %PASSWORD%
 
