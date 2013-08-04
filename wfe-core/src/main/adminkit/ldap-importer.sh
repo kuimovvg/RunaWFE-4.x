213fb7bda4d52beff0592fda00ebd3a21bfaf960
gritsenko_s <gritsenko_s@999c22fd-c5eb-e245-b5be-d41808c6d2cd>
2013-08-04 04:42:12 +0000

    guava version updated to 14.0.1
    git-svn-id: svn://alcomputer/RunaWFE-4.x/trunk/projects/wfe@4984 999c22fd-c5eb-e245-b5be-d41808c6d2cd

--- wfe-core/src/main/adminkit/ldap-importer.sh
+++ wfe-core/src/main/adminkit/ldap-importer.sh
@@ -5,6 +5,6 @@ LOGIN="Administrator"
 PASSWORD="wf"
 ########End of Configuration###############
 
-CLASSPATH="./conf:./lib/wfe.service.jar:./lib/wfe.core.jar:./lib/jbossall-client.jar:./lib/commons-logging-1.1.0.jar:./lib/commons-io-1.2.jar:./lib/guava-12.0.jar"
+CLASSPATH="./conf:./lib/wfe.service.jar:./lib/wfe.core.jar:./lib/jbossall-client.jar:./lib/commons-logging-1.1.0.jar:./lib/commons-io-1.2.jar:./lib/guava-14.0.1.jar"
 
 java -cp ${CLASSPATH} ru.runa.wfe.service.client.LDAPImporterClient ${LOGIN} ${PASSWORD}
