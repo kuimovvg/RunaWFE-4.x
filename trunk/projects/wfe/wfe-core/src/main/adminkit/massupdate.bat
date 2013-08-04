94ecdf39e26c48db35a06f8c2f51547e94ea712a
gritsenko_s <gritsenko_s@999c22fd-c5eb-e245-b5be-d41808c6d2cd>
2013-08-04 04:42:12 +0000

    guava version updated to 14.0.1
    git-svn-id: svn://alcomputer/RunaWFE-4.x@4984 999c22fd-c5eb-e245-b5be-d41808c6d2cd

--- trunk/projects/wfe/wfe-core/src/main/adminkit/massupdate.bat
+++ trunk/projects/wfe/wfe-core/src/main/adminkit/massupdate.bat
@@ -8,7 +8,7 @@ set PASSWORD="wf"
 REM ########End of Configuration###############
 
 
-set CLASSPATH=".\conf;.\lib\wfe-service.jar;.\lib\wfe-core.jar;.\lib\jbossall-client.jar;.\lib\commons-logging-1.1.0.jar;.\lib\commons-io-1.2.jar;.\lib\guava-12.0.jar;./lib/dom4j-1.6.1.jar"
+set CLASSPATH=".\conf;.\lib\wfe-service.jar;.\lib\wfe-core.jar;.\lib\jbossall-client.jar;.\lib\commons-logging-1.1.0.jar;.\lib\commons-io-1.2.jar;.\lib\guava-14.0.1.jar;./lib/dom4j-1.6.1.jar"
 
 java -cp %CLASSPATH% ru.runa.wfe.service.client.ProcessDefinitionsMassUpdate massupdate %LOGIN% %PASSWORD%
 
