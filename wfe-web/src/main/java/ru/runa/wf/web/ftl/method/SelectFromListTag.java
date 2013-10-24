0fad693961b0ca6bcd6bbc5cab756b0581a56376
gritsenko_s <gritsenko_s@999c22fd-c5eb-e245-b5be-d41808c6d2cd>
2013-10-24 09:20:25 +0000

    added tags SelectFromList, MultipleSelectFromList; wfe connection provider is set to jboss7.ws by default; minor bugfixes
    git-svn-id: svn://alcomputer/RunaWFE-4.x/trunk/projects/wfe@5197 999c22fd-c5eb-e245-b5be-d41808c6d2cd

---
+++ wfe-web/src/main/java/ru/runa/wf/web/ftl/method/SelectFromListTag.java
@@ -0,0 +1,71 @@
+package ru.runa.wf.web.ftl.method;
+
+import java.util.List;
+
+import ru.runa.wfe.commons.ftl.FreemarkerTag;
+import ru.runa.wfe.commons.ftl.FtlTagVariableHandler;
+import ru.runa.wfe.user.Executor;
+import ru.runa.wfe.var.ISelectable;
+
+import com.google.common.base.Objects;
+import com.google.common.collect.Lists;
+
+import freemarker.template.TemplateModelException;
+
+@SuppressWarnings("unchecked")
+public class SelectFromListTag extends FreemarkerTag implements FtlTagVariableHandler {
+    private static final long serialVersionUID = 1L;
+
+    @Override
+    protected Object executeTag() throws TemplateModelException {
+        String variableName = getParameterAs(String.class, 0);
+        List<Object> list = getParameterVariable(List.class, 1, null);
+        if (list == null) {
+            list = Lists.newArrayList();
+        }
+        if (list.size() > 0 && list.get(0) instanceof ISelectable) {
+            registerVariableHandler(variableName);
+        }
+        Object selectedValue = variableProvider.getValue(Object.class, variableName);
+        StringBuffer html = new StringBuffer();
+        html.append("<select name=\"").append(variableName).append("\">");
+        html.append("<option value=\"\"> ------------------------- </option>");
+        for (Object option : list) {
+            String optionValue;
+            String optionLabel;
+            if (option instanceof ISelectable) {
+                ISelectable selectable = (ISelectable) option;
+                optionValue = selectable.getValue();
+                optionLabel = selectable.getLabel();
+            } else if (option instanceof Executor) {
+                Executor executor = (Executor) option;
+                optionValue = "ID"+executor.getId();
+                optionLabel = executor.getLabel();
+            } else {
+                optionValue = String.valueOf(option);
+                optionLabel = String.valueOf(option);
+            }
+            html.append("<option value=\"").append(optionValue).append("\"");
+            if (Objects.equal(selectedValue, option)) {
+                html.append(" selected=\"true\"");
+            }
+            html.append(">").append(optionLabel).append("</option>");
+        }
+        html.append("</select>");
+        return html;
+    }
+
+    @Override
+    public Object handle(Object source) throws TemplateModelException {
+        if (source instanceof String) {
+            String value = (String) source;
+            List<ISelectable> list = getParameterVariableNotNull(List.class, 1);
+            for (ISelectable option : list) {
+                if (Objects.equal(value, option.getValue())) {
+                    return option;
+                }
+            }
+        }
+        return source;
+    }
+}
