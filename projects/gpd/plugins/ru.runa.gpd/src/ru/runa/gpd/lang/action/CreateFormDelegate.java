e9b7264148f069f7ea1fa639479876b4e11c04e0
gritsenko_s <gritsenko_s@999c22fd-c5eb-e245-b5be-d41808c6d2cd>
2013-11-25 15:24:05 +0000

    copy form from another state
    git-svn-id: svn://alcomputer/RunaWFE-4.x/trunk@5267 999c22fd-c5eb-e245-b5be-d41808c6d2cd

--- projects/gpd/plugins/ru.runa.gpd/src/ru/runa/gpd/lang/action/CreateFormDelegate.java
+++ projects/gpd/plugins/ru.runa.gpd/src/ru/runa/gpd/lang/action/CreateFormDelegate.java
@@ -4,23 +4,21 @@ import org.eclipse.core.resources.IFile;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.jface.action.IAction;
 import org.eclipse.jface.window.Window;
-import org.eclipse.ui.PlatformUI;
 
 import ru.runa.gpd.Localization;
 import ru.runa.gpd.PluginLogger;
-import ru.runa.gpd.editor.gef.command.FormNodeSetFileCommand;
 import ru.runa.gpd.form.FormTypeProvider;
 import ru.runa.gpd.lang.model.FormNode;
 import ru.runa.gpd.ui.custom.Dialogs;
 import ru.runa.gpd.ui.dialog.ChooseFormTypeDialog;
 import ru.runa.gpd.util.IOUtils;
 
-public class CreateFormDelegate extends BaseModelActionDelegate {
+public class CreateFormDelegate extends FormDelegate {
     @Override
     public void run(IAction action) {
         try {
             FormNode formNode = getSelection();
-            ChooseFormTypeDialog chooseFormTypeDialog = new ChooseFormTypeDialog(PlatformUI.getWorkbench().getDisplay().getActiveShell());
+            ChooseFormTypeDialog chooseFormTypeDialog = new ChooseFormTypeDialog();
             if (chooseFormTypeDialog.open() != Window.OK) {
                 return;
             }
@@ -35,26 +33,11 @@ public class CreateFormDelegate extends BaseModelActionDelegate {
                 file = IOUtils.createFileSafely(file);
             }
             setNewFormFile(formNode, file.getName());
-            formNode.setDirty();
-            // open form
-            OpenFormEditorDelegate openFormEditorDelegate;
-            if (ChooseFormTypeDialog.EDITOR_EXTERNAL.equals(chooseFormTypeDialog.getEditorType())) {
-                openFormEditorDelegate = new OpenExternalFormEditorDelegate();
-            } else {
-                openFormEditorDelegate = new OpenVisualFormEditorDelegate();
-            }
-            initModelActionDelegate(openFormEditorDelegate);
-            openFormEditorDelegate.run(action);
+            openForm(action, chooseFormTypeDialog.getEditorType());
         } catch (CoreException e) {
             PluginLogger.logError(e);
             throw new RuntimeException(e);
         }
     }
 
-    private void setNewFormFile(FormNode formNode, String fileName) {
-        FormNodeSetFileCommand command = new FormNodeSetFileCommand();
-        command.setFormNode(formNode);
-        command.setFileName(fileName);
-        executeCommand(command);
-    }
 }
