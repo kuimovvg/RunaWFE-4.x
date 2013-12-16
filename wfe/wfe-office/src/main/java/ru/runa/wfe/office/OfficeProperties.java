2a61da7689a1ba4588846e4e7d0945f0a769b786
gritsenko_s <gritsenko_s@999c22fd-c5eb-e245-b5be-d41808c6d2cd>
2013-12-16 12:06:35 +0000

    changed default placeholders
    git-svn-id: svn://alcomputer/RunaWFE-4.x/trunk/projects@5305 999c22fd-c5eb-e245-b5be-d41808c6d2cd

--- wfe/wfe-office/src/main/java/ru/runa/wfe/office/OfficeProperties.java
+++ wfe/wfe-office/src/main/java/ru/runa/wfe/office/OfficeProperties.java
@@ -6,11 +6,11 @@ public class OfficeProperties {
     private static final PropertyResources RESOURCES = new PropertyResources("office.properties", false);
 
     public static String getDocxPlaceholderStart() {
-        return RESOURCES.getStringProperty("docx.placeholder.start", "<");
+        return RESOURCES.getStringProperty("docx.placeholder.start", "${");
     }
 
     public static String getDocxPlaceholderEnd() {
-        return RESOURCES.getStringProperty("docx.placeholder.end", ">");
+        return RESOURCES.getStringProperty("docx.placeholder.end", "}");
     }
 
     public static String getDocxElementStart() {
