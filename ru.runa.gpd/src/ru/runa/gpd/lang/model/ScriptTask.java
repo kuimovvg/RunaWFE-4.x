d5bba00f45fd782548f1d56d6cc86118ea0dc25d
gritsenko_s <gritsenko_s@999c22fd-c5eb-e245-b5be-d41808c6d2cd>
2013-12-19 13:49:03 +0000

    ScriptTask allows only 1 output transition; Transition name is hidden in exclusive gateway in conjunction mode
    git-svn-id: svn://alcomputer/RunaWFE-4.x/trunk/projects/gpd/plugins@5312 999c22fd-c5eb-e245-b5be-d41808c6d2cd

--- ru.runa.gpd/src/ru/runa/gpd/lang/model/ScriptTask.java
+++ ru.runa.gpd/src/ru/runa/gpd/lang/model/ScriptTask.java
@@ -1,5 +1,7 @@
 package ru.runa.gpd.lang.model;
 
+import java.util.List;
+
 import ru.runa.gpd.Localization;
 import ru.runa.gpd.extension.HandlerArtifact;
 import ru.runa.gpd.extension.LocalizationRegistry;
@@ -17,6 +19,11 @@ public class ScriptTask extends Node implements Delegable {
             className = Localization.getString("label.new");
         }
         return LocalizationRegistry.getLabel(className);
+    }
+
+    @Override
+    protected boolean allowLeavingTransition(List<Transition> transitions) {
+        return super.allowLeavingTransition(transitions) && transitions.size() == 0;
     }
 
     @Override
