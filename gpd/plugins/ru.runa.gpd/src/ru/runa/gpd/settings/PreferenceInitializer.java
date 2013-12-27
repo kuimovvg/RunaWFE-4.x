a0ca464fa002c2a9bea0fe9516c56c918acfb57c
gritsenko_s <gritsenko_s@999c22fd-c5eb-e245-b5be-d41808c6d2cd>
2013-12-27 16:05:47 +0000

    actualized settings
    git-svn-id: svn://alcomputer/RunaWFE-4.x/branches/v4.0.6@5332 999c22fd-c5eb-e245-b5be-d41808c6d2cd

--- gpd/plugins/ru.runa.gpd/src/ru/runa/gpd/settings/PreferenceInitializer.java
+++ gpd/plugins/ru.runa.gpd/src/ru/runa/gpd/settings/PreferenceInitializer.java
@@ -5,7 +5,6 @@ import org.eclipse.jface.preference.IPreferenceStore;
 
 import ru.runa.gpd.Activator;
 import ru.runa.gpd.lang.Language;
-import ru.runa.wfe.commons.SystemProperties;
 
 /**
  * Class used to initialize default preference values.
@@ -19,11 +18,11 @@ public class PreferenceInitializer extends AbstractPreferenceInitializer impleme
         store.setDefault(P_FORM_DEFAULT_FCK_EDITOR, FORM_FCK_EDITOR);
         store.setDefault(P_FORM_EXTERNAL_EDITOR_PATH, "");
         store.setDefault(P_FORM_USE_EXTERNAL_EDITOR, false);
-        //store.setDefault(P_TASKS_TIMEOUT_ENABLED, false);
-        store.setDefault(P_WFE_CONNECTION_TYPE, "jboss4.rmi");
+        // store.setDefault(P_TASKS_TIMEOUT_ENABLED, false);
+        store.setDefault(P_WFE_CONNECTION_TYPE, "jboss7.ws");
         store.setDefault(P_WFE_CONNECTION_HOST, "localhost");
-        store.setDefault(P_WFE_CONNECTION_PORT, "10099");
-        store.setDefault(P_WFE_CONNECTION_VERSION, SystemProperties.getVersion());
+        store.setDefault(P_WFE_CONNECTION_PORT, "8080");
+        store.setDefault(P_WFE_CONNECTION_VERSION, "4.0.6");
         store.setDefault(P_WFE_CONNECTION_LOGIN_MODE, LOGIN_MODE_LOGIN_PASSWORD);
         store.setDefault(P_WFE_CONNECTION_LOGIN, "Administrator");
         store.setDefault(P_WFE_CONNECTION_PASSWORD, "wf");
