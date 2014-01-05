40931acb21b8345a950a84073882e485d81ce784
kanaal <kanaal@999c22fd-c5eb-e245-b5be-d41808c6d2cd>
2014-01-05 15:41:35 +0000

    Removed autogenerated content, tests change
    git-svn-id: svn://alcomputer/RunaWFE-4.x/trunk@5347 999c22fd-c5eb-e245-b5be-d41808c6d2cd

--- projects/wfe/wfe-webservice-client/src/test/java/StartProcess.java
+++ projects/wfe/wfe-webservice-client/src/test/java/StartProcess.java
@@ -9,7 +9,7 @@ import ru.runa.wfe.webservice.AuthenticationWebService;
 import ru.runa.wfe.webservice.ExecutionAPI;
 import ru.runa.wfe.webservice.ExecutionWebService;
 import ru.runa.wfe.webservice.User;
-import ru.runa.wfe.webservice.WfVariable;
+import ru.runa.wfe.webservice.Variable;
 
 public class StartProcess {
 
@@ -18,23 +18,23 @@ public class StartProcess {
             AuthenticationAPI authenticationAPI = new AuthenticationWebService().getAuthenticationAPIPort();
             User user = authenticationAPI.authenticateByLoginPassword("Administrator", "wf");
             ExecutionAPI executionAPI = new ExecutionWebService().getExecutionAPIPort();
-            List<WfVariable> variables = new ArrayList<WfVariable>();
+            List<Variable> variables = new ArrayList<Variable>();
 
-            WfVariable variable1 = new WfVariable();
+/*            Variable variable1 = new Variable();
             variable1.setName("var1");
             GregorianCalendar c = new GregorianCalendar();
             variable1.setDateValue(DatatypeFactory.newInstance().newXMLGregorianCalendar(c));
             variables.add(variable1);
 
-            WfVariable variable2 = new WfVariable();
+            Variable variable2 = new Variable();
             variable2.setName("Переменная1");
             variable2.setStringValue("СТРОКА");
             variables.add(variable2);
 
-            WfVariable variable3 = new WfVariable();
+            Variable variable3 = new Variable();
             variable3.setName("Переменная2");
             variable3.setLongValue(-3L);
-            variables.add(variable3);
+            variables.add(variable3);*/
 
             Long processId = executionAPI.startProcessWS(user, "ESCALATIONTEST", variables);
             System.out.println(processId);
