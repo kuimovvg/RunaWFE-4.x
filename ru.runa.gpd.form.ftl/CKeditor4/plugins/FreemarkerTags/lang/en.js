6649b3cc6503e548a3101b8fc8c1c2594c4e3052
gritsenko_s <gritsenko_s@999c22fd-c5eb-e245-b5be-d41808c6d2cd>
2014-01-09 10:30:36 +0000

    Added CKeditor 4.3.1 in experimental mode (for IE11)
    git-svn-id: svn://alcomputer/RunaWFE-4.x/trunk/projects/gpd/plugins@5357 999c22fd-c5eb-e245-b5be-d41808c6d2cd

---
+++ ru.runa.gpd.form.ftl/CKeditor4/plugins/FreemarkerTags/lang/en.js
@@ -0,0 +1,15 @@
+CKEDITOR.plugins.setLang(
+	'FreemarkerTags',
+	'en',
+	{
+		FreemarkerTags:
+		{
+			OutputTitle	: 'Output variable',
+			MethodTitle	: 'Freemarker tag',
+			FtlVariable : 'Variable',
+			FtlMethodParameters : 'Parameters',
+			FtlFormat : 'Variable format',
+			FtlMethod : 'Tag'
+		}
+	}
+);
