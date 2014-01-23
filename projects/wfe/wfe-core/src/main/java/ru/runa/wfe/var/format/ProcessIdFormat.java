9fdaae2ea15b44084c513775af2b113cdfb94b55
gritsenko_s <gritsenko_s@999c22fd-c5eb-e245-b5be-d41808c6d2cd>
2014-01-23 22:09:06 +0000

    added ProcessIdFormat to reference process
    git-svn-id: svn://alcomputer/RunaWFE-4.x/trunk@5422 999c22fd-c5eb-e245-b5be-d41808c6d2cd

---
+++ projects/wfe/wfe-core/src/main/java/ru/runa/wfe/var/format/ProcessIdFormat.java
@@ -0,0 +1,44 @@
+package ru.runa.wfe.var.format;
+
+import java.util.HashMap;
+
+import org.apache.commons.logging.LogFactory;
+
+import ru.runa.wfe.commons.ApplicationContextFactory;
+import ru.runa.wfe.commons.web.WebHelper;
+import ru.runa.wfe.security.Permission;
+import ru.runa.wfe.user.User;
+
+import com.google.common.collect.Maps;
+
+public class ProcessIdFormat extends LongFormat implements VariableDisplaySupport {
+
+    @Override
+    public String getName() {
+        return "processref";
+    }
+
+    @Override
+    public String formatHtml(User user, WebHelper webHelper, Long currentProcessId, String name, Object object, Object context) {
+        Long processId = (Long) object;
+        boolean link = false;
+        String title;
+        try {
+            ru.runa.wfe.execution.Process process = ApplicationContextFactory.getProcessDAO().getNotNull(processId);
+            link = ApplicationContextFactory.getPermissionDAO().isAllowed(user, Permission.READ, process);
+            title = ApplicationContextFactory.getDeploymentDAO().getNotNull(process.getDeployment().getId()).getName();
+        } catch (Exception e) {
+            title = e.toString();
+            LogFactory.getLog(getClass()).warn("Unable to determine permission or process name", e);
+        }
+        if (link) {
+            HashMap<String, Object> params = Maps.newHashMap();
+            params.put("id", processId);
+            String href = webHelper.getActionUrl("/manage_process", params);
+            return "<a href=\"" + href + "\" title=\"" + title + "\">" + processId + "</a>";
+        } else {
+            return "<span title=\"" + title + "\">" + processId + "</span>";
+        }
+    }
+
+}
