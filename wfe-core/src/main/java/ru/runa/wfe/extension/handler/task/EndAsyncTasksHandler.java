3994ef838b9fa398eec32383b980f144eb9ed670
gritsenko_s <gritsenko_s@999c22fd-c5eb-e245-b5be-d41808c6d2cd>
2014-01-23 18:49:20 +0000

    Added new handler to add read process permissions
    git-svn-id: svn://alcomputer/RunaWFE-4.x/trunk/projects/wfe@5417 999c22fd-c5eb-e245-b5be-d41808c6d2cd

--- wfe-core/src/main/java/ru/runa/wfe/extension/handler/task/EndAsyncTasksHandler.java
+++ wfe-core/src/main/java/ru/runa/wfe/extension/handler/task/EndAsyncTasksHandler.java
@@ -3,29 +3,19 @@ package ru.runa.wfe.extension.handler.task;
 import java.util.List;
 
 import ru.runa.wfe.execution.ExecutionContext;
-import ru.runa.wfe.extension.ActionHandlerBase;
-import ru.runa.wfe.extension.handler.ParamsDef;
+import ru.runa.wfe.extension.handler.ParamBasedHandlerActionHandler;
 import ru.runa.wfe.lang.BaseTaskNode;
 import ru.runa.wfe.task.Task;
 import ru.runa.wfe.task.TaskCompletionInfo;
 
 import com.google.common.base.Objects;
-import com.google.common.base.Preconditions;
 import com.google.common.collect.Lists;
 
-public class EndAsyncTasksHandler extends ActionHandlerBase {
-    private ParamsDef paramsDef;
-
-    @Override
-    public void setConfiguration(String configuration) {
-        super.setConfiguration(configuration);
-        paramsDef = ParamsDef.parse(configuration);
-    }
+public class EndAsyncTasksHandler extends ParamBasedHandlerActionHandler {
 
     @Override
     public void execute(ExecutionContext context) throws Exception {
         String nodeId = paramsDef.getInputParamValueNotNull("nodeId", context.getVariableProvider());
-        Preconditions.checkNotNull(nodeId, "nodeId is not set");
         BaseTaskNode taskNode = (BaseTaskNode) context.getProcessDefinition().getNodeNotNull(nodeId);
         if (!taskNode.isAsync()) {
             throw new IllegalArgumentException("This handler can end only async tasks");
