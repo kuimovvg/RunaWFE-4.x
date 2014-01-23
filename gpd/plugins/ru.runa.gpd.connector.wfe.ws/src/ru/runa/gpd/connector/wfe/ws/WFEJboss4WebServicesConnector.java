225be243b1670cd135883736c564d81073746acc
gritsenko_s <gritsenko_s@999c22fd-c5eb-e245-b5be-d41808c6d2cd>
2014-01-23 23:10:48 +0000

    added auto-determine version feature: http://runawfe.org/doc/WebServicesDeveloperGuide#DetermineServerVersion
    git-svn-id: svn://alcomputer/RunaWFE-4.x/trunk/projects@5427 999c22fd-c5eb-e245-b5be-d41808c6d2cd

--- gpd/plugins/ru.runa.gpd.connector.wfe.ws/src/ru/runa/gpd/connector/wfe/ws/WFEJboss4WebServicesConnector.java
+++ gpd/plugins/ru.runa.gpd.connector.wfe.ws/src/ru/runa/gpd/connector/wfe/ws/WFEJboss4WebServicesConnector.java
@@ -11,7 +11,7 @@ public class WFEJboss4WebServicesConnector extends AbstractWebServicesConnector 
         try {
             String host = Activator.getPrefString(P_WFE_CONNECTION_HOST);
             String port = Activator.getPrefString(P_WFE_CONNECTION_PORT);
-            String version = Activator.getPrefString(P_WFE_CONNECTION_VERSION);
+            String version = getVersion();
             return new URL("http://" + host + ":" + port + "/runawfe-wfe-service-" + version + "/" + serviceName + "ServiceBean?wsdl");
         } catch (MalformedURLException e) {
             throw new RuntimeException(e);
