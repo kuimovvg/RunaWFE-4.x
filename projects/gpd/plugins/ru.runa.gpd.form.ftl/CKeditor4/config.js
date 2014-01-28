56c257f088ca9bd01ef654958d6a0b08b979899a
gritsenko_s <gritsenko_s@999c22fd-c5eb-e245-b5be-d41808c6d2cd>
2014-01-28 17:08:36 +0000

    CKeditor4: added FTL tags
    git-svn-id: svn://alcomputer/RunaWFE-4.x/trunk@5448 999c22fd-c5eb-e245-b5be-d41808c6d2cd

--- projects/gpd/plugins/ru.runa.gpd.form.ftl/CKeditor4/config.js
+++ projects/gpd/plugins/ru.runa.gpd.form.ftl/CKeditor4/config.js
@@ -8,9 +8,28 @@ CKEDITOR.editorConfig = function( config ) {
 	// config.language = 'fr';
 	// config.uiColor = '#AADC6E';
 	
-	//config.extraPlugins = "FreemarkerTags";
+	config.extraPlugins = "FreemarkerTags";
 	config.contentsCss = "form.css";
 	config.bodyClass = "taskform";
 	config.height = "500px";
+	config.allowedContent = true;
+
+	config.toolbar = "Runa";
+	config.toolbar_Runa=[
+		['Source','-','NewPage','-','Templates'],
+		['Cut','Copy','Paste','PasteText','PasteFromWord','-','Print','SpellChecker','Scayt'],
+		['Undo','Redo','-','Find','Replace','-','SelectAll','RemoveFormat'],
+		['Form','Checkbox','Radio','TextField','Textarea','Select','Button','ImageButton','HiddenField'],
+		['FreemarkerMethod'],
+		'/',
+		['Bold','Italic','Underline','Strike','-','Subscript','Superscript'],
+		['NumberedList','BulletedList','-','Outdent','Indent','Blockquote'],
+		['JustifyLeft','JustifyCenter','JustifyRight','JustifyBlock'],
+		['Link','Unlink','Anchor'],
+		['Image','Flash','Table','HorizontalRule','Smiley','SpecialChar','PageBreak'],
+		'/',
+		['Styles','Format','Font','FontSize'],
+		['TextColor','BGColor'],['Maximize','ShowBlocks','-','About']
+	];
 	
 };
--- projects/gpd/plugins/ru.runa.gpd.form.ftl/CKeditor4/plugins/FreemarkerTags/lang/en.js
+++ projects/gpd/plugins/ru.runa.gpd.form.ftl/CKeditor4/plugins/FreemarkerTags/lang/en.js
@@ -1,15 +1,3 @@
-CKEDITOR.plugins.setLang(
-	'FreemarkerTags',
-	'en',
-	{
-		FreemarkerTags:
-		{
-			OutputTitle	: 'Output variable',
-			MethodTitle	: 'Freemarker tag',
-			FtlVariable : 'Variable',
-			FtlMethodParameters : 'Parameters',
-			FtlFormat : 'Variable format',
-			FtlMethod : 'Tag'
-		}
-	}
-);
+CKEDITOR.plugins.setLang("FreemarkerTags", "en", {
+	MethodTitle: "Freemarker tag"
+});
