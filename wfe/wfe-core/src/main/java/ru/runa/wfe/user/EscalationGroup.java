71c86f87f041edc590c7e78e73efc747f0157077
gritsenko_s <gritsenko_s@999c22fd-c5eb-e245-b5be-d41808c6d2cd>
2014-02-13 13:53:41 +0000

    fixed: org.hibernate.PropertyValueException: not-null property references a null or transient value: ru.runa.wfe.user.EscalationGroup.createDate
    git-svn-id: svn://alcomputer/RunaWFE-4.x/trunk/projects@5497 999c22fd-c5eb-e245-b5be-d41808c6d2cd

--- wfe/wfe-core/src/main/java/ru/runa/wfe/user/EscalationGroup.java
+++ wfe/wfe-core/src/main/java/ru/runa/wfe/user/EscalationGroup.java
@@ -1,5 +1,7 @@
 package ru.runa.wfe.user;
 
+import java.util.Date;
+
 import javax.persistence.Column;
 import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
@@ -54,6 +56,7 @@ public class EscalationGroup extends TemporaryGroup {
     public static EscalationGroup create(Process process, Task task, Executor originalExecutor, int escalationLevel) {
         String identifier = GROUP_PREFIX + "_" + process.getId() + "_" + task.getId();
         EscalationGroup escalationGroup = new EscalationGroup();
+        escalationGroup.setCreateDate(new Date());
         escalationGroup.setName(identifier);
         escalationGroup.setDescription(process.getId().toString());
         escalationGroup.setOriginalExecutor(originalExecutor);
