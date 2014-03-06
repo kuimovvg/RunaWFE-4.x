1ad2907611969cfeef012301b0f6c9f7123b7bda
gritsenko_s <gritsenko_s@999c22fd-c5eb-e245-b5be-d41808c6d2cd>
2014-03-06 15:29:36 +0000

    for jboss7: wss4j-axis added due to version change 1.5.4 -> 1.6.5;
    git-svn-id: svn://alcomputer/RunaWFE-4.x/trunk@5525 999c22fd-c5eb-e245-b5be-d41808c6d2cd

--- projects/wfe/wfe-app/repository/add_dependencies.bat
+++ projects/wfe/wfe-app/repository/add_dependencies.bat
@@ -5,5 +5,6 @@ CALL mvn install:install-file -Dfile=jacob-1.0.jar -DgroupId=com.activex -Dartif
 CALL mvn install:install-file -Dfile=jcom-1.0.jar -DgroupId=com.activex -DartifactId=jcom -Dversion=1.0 -Dpackaging=jar -DgeneratePom=true
 CALL mvn install:install-file -Dfile=cglib-3.0.jar -DgroupId=cglib -DartifactId=cglib -Dversion=3.0 -Dpackaging=jar -DgeneratePom=true
 CALL mvn install:install-file -Dfile=alfresco-web-service-client-3.2.r2.jar -DgroupId=org.alfresco -DartifactId=alfresco-web-service-client -Dversion=3.2r2 -Dpackaging=jar -DgeneratePom=true
+CALL mvn install:install-file -Dfile=wss4j-axis-1.6.5.jar -DgroupId=org.apache.ws.security -DartifactId=wss4j-axis -Dversion=1.6.5 -Dpackaging=jar -DgeneratePom=true
 
 pause
