f5dc40e80e7026693f05329bca989cd1e944062b
gritsenko_s <gritsenko_s@999c22fd-c5eb-e245-b5be-d41808c6d2cd>
2014-03-14 15:44:18 +0000

    fixed SnapToGrid function in BPMN editor
    git-svn-id: svn://alcomputer/RunaWFE-4.x/trunk@5534 999c22fd-c5eb-e245-b5be-d41808c6d2cd

--- projects/gpd/plugins/ru.runa.gpd/src/ru/runa/gpd/editor/gef/GEFProcessEditor.java
+++ projects/gpd/plugins/ru.runa.gpd/src/ru/runa/gpd/editor/gef/GEFProcessEditor.java
@@ -2,6 +2,7 @@ package ru.runa.gpd.editor.gef;
 
 import java.beans.PropertyChangeEvent;
 
+import org.eclipse.gef.SnapToGrid;
 import org.eclipse.gef.ui.parts.GraphicalEditor;
 
 import ru.runa.gpd.editor.ProcessEditorBase;
@@ -27,5 +28,12 @@ public class GEFProcessEditor extends ProcessEditorBase {
         if (PropertyNames.PROPERTY_SHOW_ACTIONS.equals(evt.getPropertyName())) {
             ((DesignerGraphicalEditorPart) graphPage).getPaletteRoot().refreshElementsVisibility();
         }
+    }
+    
+    @Override
+    protected void updateGridLayerVisibility(boolean enabled) {
+        getGraphicalViewer().setProperty(SnapToGrid.PROPERTY_GRID_ENABLED, enabled);
+        getGraphicalViewer().setProperty(SnapToGrid.PROPERTY_GRID_VISIBLE, enabled);
+        refresh();
     }
 }
