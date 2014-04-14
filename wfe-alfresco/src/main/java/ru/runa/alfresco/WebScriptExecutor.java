56ffacd2558d6f14992efb46256f8f0b613dc488
gritsenko_s <gritsenko_s@999c22fd-c5eb-e245-b5be-d41808c6d2cd>
2014-04-14 20:01:08 +0000

    duplicated code removed
    git-svn-id: svn://alcomputer/RunaWFE-4.x/trunk/projects/wfe@5599 999c22fd-c5eb-e245-b5be-d41808c6d2cd

--- wfe-alfresco/src/main/java/ru/runa/alfresco/WebScriptExecutor.java
+++ wfe-alfresco/src/main/java/ru/runa/alfresco/WebScriptExecutor.java
@@ -1,9 +1,9 @@
 package ru.runa.alfresco;
 
 import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
 import java.net.URLEncoder;
 import java.nio.charset.Charset;
-import java.util.HashMap;
 import java.util.Map;
 
 import org.apache.commons.logging.Log;
@@ -36,16 +36,11 @@ public class WebScriptExecutor {
     private boolean useHttpPost;
     private boolean throwExceptionOnErrorState = true;
     private final String webScriptUri;
-    private final Map<String, String> webScriptParameters;
-    private Map<String, FileVariable> webScriptFileParameters = new HashMap<String, FileVariable>();
+    private final Map<String, Object> webScriptParameters;
 
-    public WebScriptExecutor(String webScriptUri, Map<String, String> webScriptParameters) {
+    public WebScriptExecutor(String webScriptUri, Map<String, Object> webScriptParameters) {
         this.webScriptUri = webScriptUri;
         this.webScriptParameters = webScriptParameters;
-    }
-
-    public void setWebScriptFileParameters(Map<String, FileVariable> webScriptFileParameters) {
-        this.webScriptFileParameters = webScriptFileParameters;
     }
 
     public void setUseHttpPost(boolean useHttpPost) {
@@ -56,54 +51,27 @@ public class WebScriptExecutor {
         this.throwExceptionOnErrorState = throwExceptionOnErrorState;
     }
 
-    public boolean isUseHttpPost() {
-        return useHttpPost;
-    }
-
-    public boolean isThrowExceptionOnErrorState() {
-        return throwExceptionOnErrorState;
-    }
-
-    public String getWebScriptUri() {
-        return webScriptUri;
-    }
-
-    public Map<String, String> getWebScriptParameters() {
-        return webScriptParameters;
-    }
-
-    public Map<String, FileVariable> getWebScriptFileParameters() {
-        return webScriptFileParameters;
-    }
-
-    public ByteArrayOutputStream doRequest() {
+    public byte[] doRequest() {
         try {
-            final DefaultHttpClient httpClient = new DefaultHttpClient();
+            DefaultHttpClient httpClient = new DefaultHttpClient();
             httpClient.getCredentialsProvider().setCredentials(new AuthScope(null, -1, null),
                     new UsernamePasswordCredentials(WSConnectionSettings.getInstance().getLogin(), WSConnectionSettings.getInstance().getPassword()));
             String alfBaseUrl = WSConnectionSettings.getInstance().getAlfBaseUrl();
-            final HttpUriRequest request;
+            HttpUriRequest request;
             if (useHttpPost) {
                 request = formHttpPostRequest(alfBaseUrl);
             } else {
                 request = formHttpGetRequest(alfBaseUrl);
             }
-            return new RemoteAlfConnector<ByteArrayOutputStream>() {
-
-                @Override
-                protected ByteArrayOutputStream code() throws Exception {
-                    HttpResponse response = httpClient.execute(request);
-                    int statusCode = response.getStatusLine().getStatusCode();
-                    log.debug("WebScript status code = " + statusCode);
-                    if (statusCode != 200 && throwExceptionOnErrorState) {
-                        throw new InternalApplicationException("WebScript " + request.getRequestLine() + " status code is " + statusCode);
-                    }
-                    ByteArrayOutputStream baos = new ByteArrayOutputStream();
-                    response.getEntity().writeTo(baos);
-                    return baos;
-                }
-
-            }.runInSession();
+            HttpResponse response = httpClient.execute(request);
+            int statusCode = response.getStatusLine().getStatusCode();
+            log.debug("WebScript status code = " + statusCode);
+            if (statusCode != 200 && throwExceptionOnErrorState) {
+                throw new InternalApplicationException("WebScript " + request.getRequestLine() + " status code is " + statusCode);
+            }
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            response.getEntity().writeTo(baos);
+            return baos.toByteArray();
         } catch (Exception e) {
             throw Throwables.propagate(e);
         }
@@ -113,20 +81,22 @@ public class WebScriptExecutor {
         StringBuffer url = new StringBuffer();
         url.append(alfBaseUrl).append("service/").append(webScriptUri);
         boolean first = true;
-        for (String paramName : webScriptParameters.keySet()) {
-            String paramValue = webScriptParameters.get(paramName);
-            paramValue = URLEncoder.encode(paramValue, "UTF-8");
+        for (Map.Entry<String, Object> entry : webScriptParameters.entrySet()) {
+            if (entry.getValue() == null || entry.getValue() instanceof FileVariable) {
+                log.warn("Ignored parameter " + entry.getKey() + "=" + entry.getValue());
+                continue;
+            }
+            String paramValue = URLEncoder.encode(entry.getValue().toString(), "UTF-8");
             if (first) {
                 url.append("?");
             } else {
                 url.append("&");
             }
-            url.append(paramName).append("=").append(paramValue);
+            url.append(entry.getKey()).append("=").append(paramValue);
             first = false;
         }
         log.info("Executing WebScript " + url);
-        HttpGet request = new HttpGet(url.toString());
-        return request;
+        return new HttpGet(url.toString());
     }
 
     private HttpPost formHttpPostRequest(String alfBaseUrl) throws Exception {
@@ -134,21 +104,22 @@ public class WebScriptExecutor {
         url.append(alfBaseUrl).append("service/").append(webScriptUri);
         HttpPost request = new HttpPost(url.toString());
         MultipartEntity entity = new MultipartEntity();
-
-        for (String paramName : webScriptParameters.keySet()) {
-            String paramValue = webScriptParameters.get(paramName);
-            entity.addPart(paramName, new StringBody(paramValue, Charset.forName("UTF-8")));
-        }
-
-        for (String paramName : webScriptFileParameters.keySet()) {
-            byte[] fileData = webScriptFileParameters.get(paramName).getData();
-            InputStreamBody body = new InputStreamBody(ByteStreams.newInputStreamSupplier(fileData).getInput(), webScriptFileParameters
-                    .get(paramName).getContentType(), webScriptFileParameters.get(paramName).getName());
-            entity.addPart(paramName, body);
+        for (Map.Entry<String, Object> entry : webScriptParameters.entrySet()) {
+            if (entry.getValue() == null) {
+                log.warn("Ignored null parameter " + entry.getKey());
+                continue;
+            }
+            if (entry.getValue() instanceof FileVariable) {
+                FileVariable fileVariable = (FileVariable) entry.getValue();
+                InputStream inputStream = ByteStreams.newInputStreamSupplier(fileVariable.getData()).getInput();
+                InputStreamBody body = new InputStreamBody(inputStream, fileVariable.getContentType(), fileVariable.getName());
+                entity.addPart(entry.getKey(), body);
+            } else {
+                entity.addPart(entry.getKey(), new StringBody(entry.getValue().toString(), Charset.forName("UTF-8")));
+            }
         }
         long length = entity.getContentLength();
         request.setEntity(entity);
-
         log.info("Executing WebScript via post " + url + " content-length = " + length);
         return request;
     }
