bb08ba62e8fe88ddb146cbacf1ef7360c190ef39
rivenforce <rivenforce@999c22fd-c5eb-e245-b5be-d41808c6d2cd>
2014-05-01 14:37:27 +0000

    fixed bug 591: java.lang.NullPointerException
    git-svn-id: svn://alcomputer/RunaWFE-4.x/trunk/projects/wfe@5644 999c22fd-c5eb-e245-b5be-d41808c6d2cd

--- wfe-web/src/main/java/ru/runa/wf/web/action/ImportDataFileAction.java
+++ wfe-web/src/main/java/ru/runa/wf/web/action/ImportDataFileAction.java
@@ -64,98 +64,108 @@ public class ImportDataFileAction extends ActionBase {
         FileForm fileForm = (FileForm) form;
         byte[] archive = fileForm.getFile().getFileData();
 
-        boolean clearBeforeUpload = false;
-
-        String paramType = request.getParameter(UPLOAD_PARAM);
-        if (CLEAR_BEFORE_UPLOAD.equals(paramType)) {
-            clearBeforeUpload = true;
-        }
-
-        String defaultPasswordValue = null;
-        final String passwordParamType = request.getParameter(PASSWORD_PARAM);
-        if (SET_PASSWORD.equals(passwordParamType)) {
-            defaultPasswordValue = request.getParameter(PASSWORD_VALUE_PARAM);
-        }
-
-        ZipInputStream zin = new ZipInputStream(new ByteArrayInputStream(archive));
-        Map<String, byte[]> files = new HashMap<String, byte[]>();
-        Map<String, byte[]> configs = new HashMap<String, byte[]>();
-        ZipEntry entry;
-        while ((entry = zin.getNextEntry()) != null) {
-            byte[] bytes = ByteStreams.toByteArray(zin);
-            if (entry.getName().endsWith(".conf")) {
-                configs.put(entry.getName().substring(DataFileBuilder.PATH_TO_BOTTASK.length(), entry.getName().length()), bytes);
-            } else {
-                files.put(entry.getName(), bytes);
-            }
-        }
-        byte[] scriptXml = files.remove(DataFileBuilder.PATH_TO_XML);
-
-        InputStream scriptInputStream = new ByteArrayInputStream(scriptXml);
-        Document allDocument = XmlUtils.parseWithXSDValidation(scriptInputStream, "workflowScript.xsd");
-        Element root = allDocument.getRootElement();
-        byte[][] processDefinitionsBytes = readProcessDefinitionsToByteArrays(root, files);
-
-        if (clearBeforeUpload) {
-            List<WfProcess> wfProcesses = Delegates.getExecutionService().getProcesses(getLoggedUser(request),
-                    BatchPresentationFactory.PROCESSES.createDefault());
-            ProcessFilter processFilter = new ProcessFilter();
-            for (WfProcess wfProcess : wfProcesses) {
-                processFilter.setId(wfProcess.getId());
-                Delegates.getExecutionService().removeProcesses(getLoggedUser(request), processFilter);
+        try {
+            if (archive == null || archive.length == 0) {
+                throw new DataFileNotPresentException();
             }
 
-            List<WfDefinition> definitions = Delegates.getDefinitionService().getLatestProcessDefinitions(getLoggedUser(request),
-                    BatchPresentationFactory.DEFINITIONS.createDefault());
-            for (WfDefinition definition : definitions) {
-                Delegates.getDefinitionService().undeployProcessDefinition(getLoggedUser(request), definition.getName(), null);
+            boolean clearBeforeUpload = false;
+
+            String paramType = request.getParameter(UPLOAD_PARAM);
+            if (CLEAR_BEFORE_UPLOAD.equals(paramType)) {
+                clearBeforeUpload = true;
             }
 
-            List<BotStation> botStations = Delegates.getBotService().getBotStations();
-            for (BotStation botStation : botStations) {
-                List<Bot> bots = Delegates.getBotService().getBots(getLoggedUser(request), botStation.getId());
-                for (Bot bot : bots) {
-                    List<BotTask> botTasks = Delegates.getBotService().getBotTasks(getLoggedUser(request), bot.getId());
-                    for (BotTask botTask : botTasks) {
-                        Delegates.getBotService().removeBotTask(getLoggedUser(request), botTask.getId());
+            String defaultPasswordValue = null;
+            final String passwordParamType = request.getParameter(PASSWORD_PARAM);
+            if (SET_PASSWORD.equals(passwordParamType)) {
+                defaultPasswordValue = request.getParameter(PASSWORD_VALUE_PARAM);
+            }
+
+            ZipInputStream zin = new ZipInputStream(new ByteArrayInputStream(archive));
+            Map<String, byte[]> files = new HashMap<String, byte[]>();
+            Map<String, byte[]> configs = new HashMap<String, byte[]>();
+            ZipEntry entry;
+            while ((entry = zin.getNextEntry()) != null) {
+                byte[] bytes = ByteStreams.toByteArray(zin);
+                if (entry.getName().endsWith(".conf")) {
+                    configs.put(entry.getName().substring(DataFileBuilder.PATH_TO_BOTTASK.length(), entry.getName().length()), bytes);
+                } else {
+                    files.put(entry.getName(), bytes);
+                }
+            }
+            byte[] scriptXml = files.remove(DataFileBuilder.PATH_TO_XML);
+
+            InputStream scriptInputStream = new ByteArrayInputStream(scriptXml);
+            Document allDocument = XmlUtils.parseWithXSDValidation(scriptInputStream, "workflowScript.xsd");
+            Element root = allDocument.getRootElement();
+            byte[][] processDefinitionsBytes = readProcessDefinitionsToByteArrays(root, files);
+
+            if (clearBeforeUpload) {
+                List<WfProcess> wfProcesses = Delegates.getExecutionService().getProcesses(getLoggedUser(request),
+                        BatchPresentationFactory.PROCESSES.createDefault());
+                ProcessFilter processFilter = new ProcessFilter();
+                for (WfProcess wfProcess : wfProcesses) {
+                    processFilter.setId(wfProcess.getId());
+                    Delegates.getExecutionService().removeProcesses(getLoggedUser(request), processFilter);
+                }
+
+                List<WfDefinition> definitions = Delegates.getDefinitionService().getLatestProcessDefinitions(getLoggedUser(request),
+                        BatchPresentationFactory.DEFINITIONS.createDefault());
+                for (WfDefinition definition : definitions) {
+                    Delegates.getDefinitionService().undeployProcessDefinition(getLoggedUser(request), definition.getName(), null);
+                }
+
+                List<BotStation> botStations = Delegates.getBotService().getBotStations();
+                for (BotStation botStation : botStations) {
+                    List<Bot> bots = Delegates.getBotService().getBots(getLoggedUser(request), botStation.getId());
+                    for (Bot bot : bots) {
+                        List<BotTask> botTasks = Delegates.getBotService().getBotTasks(getLoggedUser(request), bot.getId());
+                        for (BotTask botTask : botTasks) {
+                            Delegates.getBotService().removeBotTask(getLoggedUser(request), botTask.getId());
+                        }
+                        Delegates.getBotService().removeBot(getLoggedUser(request), bot.getId());
                     }
-                    Delegates.getBotService().removeBot(getLoggedUser(request), bot.getId());
+                    Delegates.getBotService().removeBotStation(getLoggedUser(request), botStation.getId());
                 }
-                Delegates.getBotService().removeBotStation(getLoggedUser(request), botStation.getId());
+
+                List<Relation> relations = Delegates.getRelationService().getRelations(getLoggedUser(request),
+                        BatchPresentationFactory.RELATIONS.createDefault());
+                for (Relation relation : relations) {
+                    List<RelationPair> relationPairs = Delegates.getRelationService().getRelationPairs(getLoggedUser(request), relation.getName(),
+                            BatchPresentationFactory.RELATION_PAIRS.createDefault());
+                    for (RelationPair relationPair : relationPairs) {
+                        Delegates.getRelationService().removeRelationPair(getLoggedUser(request), relationPair.getId());
+                    }
+                    Delegates.getRelationService().removeRelation(getLoggedUser(request), relation.getId());
+                }
+
+                List<? extends Executor> executors = Delegates.getExecutorService().getExecutors(getLoggedUser(request),
+                        BatchPresentationFactory.EXECUTORS.createDefault());
+                List<Long> ids = new ArrayList<Long>();
+                for (Executor executor : executors) {
+                    if (!ApplicationContextFactory.getPermissionDAO().isPrivilegedExecutor(executor)) {
+                        ids.add(executor.getId());
+                    }
+                }
+                Delegates.getExecutorService().remove(getLoggedUser(request), ids);
             }
 
-            List<Relation> relations = Delegates.getRelationService().getRelations(getLoggedUser(request),
-                    BatchPresentationFactory.RELATIONS.createDefault());
-            for (Relation relation : relations) {
-                List<RelationPair> relationPairs = Delegates.getRelationService().getRelationPairs(getLoggedUser(request), relation.getName(),
-                        BatchPresentationFactory.RELATION_PAIRS.createDefault());
-                for (RelationPair relationPair : relationPairs) {
-                    Delegates.getRelationService().removeRelationPair(getLoggedUser(request), relationPair.getId());
+            List<String> errors = Delegates.getScriptingService().executeAdminScriptSkipError(getLoggedUser(request), scriptXml,
+                    processDefinitionsBytes, configs, defaultPasswordValue);
+            if (errors != null && errors.size() > 0) {
+                for (String error : errors) {
+                    addError(request, new Exception(error));
                 }
-                Delegates.getRelationService().removeRelation(getLoggedUser(request), relation.getId());
+                return mapping.findForward(Resources.FORWARD_FAILURE);
             }
 
-            List<? extends Executor> executors = Delegates.getExecutorService().getExecutors(getLoggedUser(request),
-                    BatchPresentationFactory.EXECUTORS.createDefault());
-            List<Long> ids = new ArrayList<Long>();
-            for (Executor executor : executors) {
-                if (!ApplicationContextFactory.getPermissionDAO().isPrivilegedExecutor(executor)) {
-                    ids.add(executor.getId());
-                }
-            }
-            Delegates.getExecutorService().remove(getLoggedUser(request), ids);
-        }
-
-        List<String> errors = Delegates.getScriptingService().executeAdminScriptSkipError(getLoggedUser(request), scriptXml, processDefinitionsBytes,
-                configs, defaultPasswordValue);
-        if (errors != null && errors.size() > 0) {
-            for (String error : errors) {
-                addError(request, new Exception(error));
-            }
+            return mapping.findForward(Resources.FORWARD_SUCCESS);
+        } catch (Exception e) {
+            addError(request, e);
             return mapping.findForward(Resources.FORWARD_FAILURE);
         }
 
-        return mapping.findForward(Resources.FORWARD_SUCCESS);
     }
 
     private static byte[][] readProcessDefinitionsToByteArrays(Element element, Map<String, byte[]> files) throws IOException {
