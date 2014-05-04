44b773810da378c64974086aebd34bb052fb7c7e
gritsenko_s <gritsenko_s@999c22fd-c5eb-e245-b5be-d41808c6d2cd>
2014-05-04 18:02:26 +0000

    added sorting in user type tables
    git-svn-id: svn://alcomputer/RunaWFE-4.x/trunk/projects@5656 999c22fd-c5eb-e245-b5be-d41808c6d2cd

--- gpd/plugins/ru.runa.gpd/src/ru/runa/gpd/lang/model/VariableUserType.java
+++ gpd/plugins/ru.runa.gpd/src/ru/runa/gpd/lang/model/VariableUserType.java
@@ -9,87 +9,93 @@ import com.google.common.base.Preconditions;
 import com.google.common.collect.Lists;
 
 public class VariableUserType extends EventSupport implements VariableContainer, PropertyNames {
-	public static final String PREFIX = "usertype:";
+    public static final String PREFIX = "usertype:";
     public static final String DELIM = ".";
-	private String name;
-	private List<Variable> attributes = Lists.newArrayList();
-	private ProcessDefinition processDefinition;
-	
-	public VariableUserType() {
-	    
-	}
-	
-	public ProcessDefinition getProcessDefinition() {
-		return processDefinition;
-	}
-	
-	public void setProcessDefinition(ProcessDefinition processDefinition) {
-		this.processDefinition = processDefinition;
-	}
-	
-	@Override
-	public void firePropertyChange(String propName, Object old, Object newValue) {
-		super.firePropertyChange(propName, old, newValue);
-		if (processDefinition != null) {
-		    processDefinition.setDirty();
-		}
-	}
-	
-	public String getName() {
-		return name;
-	}
-	
-	public void setName(String name) {
-		String old = this.name;
-		this.name = name;
-		firePropertyChange(PROPERTY_NAME, old, name);
-	}
-	
-	public List<Variable> getAttributes() {
-		return attributes;
-	}
-	
-	public void addAttribute(Variable variable) {
-		attributes.add(variable);
-		firePropertyChange(PROPERTY_CHILDS_CHANGED, null, variable);
-	}
+    private String name;
+    private final List<Variable> attributes = Lists.newArrayList();
+    private ProcessDefinition processDefinition;
 
-	public void removeAttribute(Variable variable) {
-		attributes.remove(variable);
-		firePropertyChange(PROPERTY_CHILDS_CHANGED, null, variable);
-	}
+    public VariableUserType() {
 
-	@Override
-	public List<Variable> getVariables(boolean expandComplexTypes,
-			boolean includeSwimlanes, String... typeClassNameFilters) {
-		Preconditions.checkArgument(!expandComplexTypes, "Complex type expansion is not supported");
-		Preconditions.checkArgument(typeClassNameFilters==null || typeClassNameFilters.length==0, "Filtering is not supported");
-		return getAttributes();
-	}
-	
-	public VariableUserType getCopy() {
-	    VariableUserType type = new VariableUserType();
-	    type.name = name;
-	    type.attributes.addAll(getAttributes());
-	    return type;
-	}
+    }
 
-	@Override
-	public int hashCode() {
-	    return Objects.hashCode(name, attributes);
-	}
-	
-	@Override
-	public boolean equals(Object obj) {
-	    if (!(obj instanceof VariableUserType)) {
-	        return false;
-	    }
-	    VariableUserType type = (VariableUserType) obj;
-	    return Objects.equal(name, type.name) && Objects.equal(attributes, type.attributes);
-	}
-	
-	@Override
-	public String toString() {
-	    return Objects.toStringHelper(getClass()).add("name", name).add("attributes", attributes).toString();
-	}
+    public ProcessDefinition getProcessDefinition() {
+        return processDefinition;
+    }
+
+    public void setProcessDefinition(ProcessDefinition processDefinition) {
+        this.processDefinition = processDefinition;
+    }
+
+    @Override
+    public void firePropertyChange(String propName, Object old, Object newValue) {
+        super.firePropertyChange(propName, old, newValue);
+        if (processDefinition != null) {
+            processDefinition.setDirty();
+        }
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        String old = this.name;
+        this.name = name;
+        firePropertyChange(PROPERTY_NAME, old, name);
+    }
+
+    public List<Variable> getAttributes() {
+        return attributes;
+    }
+
+    public void addAttribute(Variable variable) {
+        attributes.add(variable);
+        firePropertyChange(PROPERTY_CHILDS_CHANGED, null, variable);
+    }
+
+    public void changeAttributePosition(Variable attribute, int position) {
+        if (position != -1 && attributes.remove(attribute)) {
+            attributes.add(position, attribute);
+            firePropertyChange(PROPERTY_CHILDS_CHANGED, null, attribute);
+        }
+    }
+
+    public void removeAttribute(Variable variable) {
+        attributes.remove(variable);
+        firePropertyChange(PROPERTY_CHILDS_CHANGED, null, variable);
+    }
+
+    @Override
+    public List<Variable> getVariables(boolean expandComplexTypes, boolean includeSwimlanes, String... typeClassNameFilters) {
+        Preconditions.checkArgument(!expandComplexTypes, "Complex type expansion is not supported");
+        Preconditions.checkArgument(typeClassNameFilters == null || typeClassNameFilters.length == 0, "Filtering is not supported");
+        return getAttributes();
+    }
+
+    public VariableUserType getCopy() {
+        VariableUserType type = new VariableUserType();
+        type.name = name;
+        type.attributes.addAll(getAttributes());
+        return type;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(name, attributes);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (!(obj instanceof VariableUserType)) {
+            return false;
+        }
+        VariableUserType type = (VariableUserType) obj;
+        return Objects.equal(name, type.name) && Objects.equal(attributes, type.attributes);
+    }
+
+    @Override
+    public String toString() {
+        return Objects.toStringHelper(getClass()).add("name", name).add("attributes", attributes).toString();
+    }
 }
