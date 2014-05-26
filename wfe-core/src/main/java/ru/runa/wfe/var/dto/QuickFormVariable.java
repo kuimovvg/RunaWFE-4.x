e19e517e54a8a3adc16db08c93ee9e1c36ad6643
gritsenko_s <gritsenko_s@999c22fd-c5eb-e245-b5be-d41808c6d2cd>
2014-05-26 15:30:49 +0000

    variable description in quick form templates added
    git-svn-id: svn://alcomputer/RunaWFE-4.x/trunk/projects/wfe@5684 999c22fd-c5eb-e245-b5be-d41808c6d2cd

--- wfe-core/src/main/java/ru/runa/wfe/var/dto/QuickFormVariable.java
+++ wfe-core/src/main/java/ru/runa/wfe/var/dto/QuickFormVariable.java
@@ -5,16 +5,9 @@ import ru.runa.wfe.commons.ftl.FreemarkerTagHelper;
 public class QuickFormVariable {
     private String tagName;
     private String name;
+    private String scriptingName;
+    private String description;
     private String[] params;
-
-    public QuickFormVariable() {
-    }
-
-    public QuickFormVariable(String tagName, String name, String... params) {
-        this.tagName = tagName;
-        this.name = name;
-        this.params = params;
-    }
 
     public String getTag() {
         return FreemarkerTagHelper.build(tagName, name, params);
@@ -26,6 +19,22 @@ public class QuickFormVariable {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public String getScriptingName() {
+        return scriptingName;
+    }
+
+    public void setScriptingName(String scriptingName) {
+        this.scriptingName = scriptingName;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description != null ? description : "";
     }
 
     public String[] getParams() {
