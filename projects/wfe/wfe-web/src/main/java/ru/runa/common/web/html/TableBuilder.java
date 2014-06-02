4e211e36e1c761dedb607692929a5cc0e460393f
gritsenko_s <gritsenko_s@999c22fd-c5eb-e245-b5be-d41808c6d2cd>
2014-06-02 11:53:54 +0000

    fix: display logs more than 1500 rows
    git-svn-id: svn://alcomputer/RunaWFE-4.x/trunk@5698 999c22fd-c5eb-e245-b5be-d41808c6d2cd

--- projects/wfe/wfe-web/src/main/java/ru/runa/common/web/html/TableBuilder.java
+++ projects/wfe/wfe-web/src/main/java/ru/runa/common/web/html/TableBuilder.java
@@ -21,14 +21,14 @@ import org.apache.ecs.html.TR;
 import org.apache.ecs.html.Table;
 
 import ru.runa.common.web.Resources;
+import ru.runa.wfe.InternalApplicationException;
 
 /**
  * Created on 10.11.2004
  * 
  */
 public class TableBuilder {
-
-    static final int max_rows = 1500;
+    static final int indefiniteLoopCheckerMaxRows = 100000;
 
     public Table build(HeaderBuilder headerBuilder, RowBuilder rowBuilder) {
         return build(headerBuilder, rowBuilder, false);
@@ -40,15 +40,23 @@ public class TableBuilder {
         table.addElement(headerBuilder.build());
         int rowsCount = 0;
         if (buildArray) {
-            while (rowBuilder.hasNext() && ++rowsCount < max_rows) {
+            while (rowBuilder.hasNext()) {
+                rowsCount++;
                 for (TR tr : rowBuilder.buildNextArray()) {
                     table.addElement(tr);
                     rowsCount++;
                 }
+                if (rowsCount > indefiniteLoopCheckerMaxRows) {
+                    throw new InternalApplicationException("Indefinite loop detected");
+                }
             }
         } else {
-            while (rowBuilder.hasNext() && ++rowsCount < max_rows) {
+            while (rowBuilder.hasNext()) {
+                rowsCount++;
                 table.addElement(rowBuilder.buildNext());
+                if (rowsCount > indefiniteLoopCheckerMaxRows) {
+                    throw new InternalApplicationException("Indefinite loop detected");
+                }
             }
         }
         return table;
