7d92672021303e28d979b82661ee35531a1a4a6b
mihaj <mihaj@999c22fd-c5eb-e245-b5be-d41808c6d2cd>
2014-06-26 06:37:01 +0000

    - Генерация регламента процесса  - freemarker.jar теперь один для все модулей
    git-svn-id: svn://alcomputer/RunaWFE-4.x/trunk/projects@5745 999c22fd-c5eb-e245-b5be-d41808c6d2cd

---
+++ gpd/plugins/ru.runa.gpd/src/ru/runa/gpd/ui/action/CreateProcessRegulation.java
@@ -0,0 +1,106 @@
+package ru.runa.gpd.ui.action;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.io.StringReader;
+import java.io.StringWriter;
+import java.io.Writer;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.text.SimpleDateFormat;
+import java.util.Collections;
+import java.util.Date;
+import java.util.HashMap;
+
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.runtime.FileLocator;
+import org.eclipse.core.runtime.Path;
+import org.eclipse.jface.action.IAction;
+import org.eclipse.ui.ide.IDE;
+import org.osgi.framework.Bundle;
+
+import ru.runa.gpd.Activator;
+import ru.runa.gpd.PluginLogger;
+import ru.runa.gpd.lang.action.BaseModelActionDelegate;
+import ru.runa.gpd.lang.model.EndState;
+import ru.runa.gpd.lang.model.EndTokenState;
+import ru.runa.gpd.lang.model.Node;
+import ru.runa.gpd.lang.model.ProcessDefinition;
+import ru.runa.gpd.lang.model.StartState;
+import ru.runa.gpd.lang.model.TaskState;
+import ru.runa.gpd.lang.model.Timer;
+import ru.runa.gpd.lang.model.Variable;
+import ru.runa.gpd.lang.par.ParContentProvider;
+import ru.runa.gpd.util.IOUtils;
+import freemarker.template.Configuration;
+import freemarker.template.ObjectWrapper;
+import freemarker.template.Template;
+import freemarker.template.TemplateExceptionHandler;
+
+public class CreateProcessRegulation extends BaseModelActionDelegate {
+
+	@Override
+	public void run(IAction action) {
+		
+		try {
+			ProcessDefinition proccDefinition = getActiveDesignerEditor().getDefinition();
+			String html = generateRegulation(proccDefinition);
+			
+			SimpleDateFormat dataFormat = new SimpleDateFormat("yyyy.mm.dd_hh-mm");
+			IFile file = IOUtils.getAdjacentFile(getDefinitionFile(), proccDefinition.getName()+"_"+dataFormat.format(new Date())+".html");
+			IOUtils.createFile(file,new ByteArrayInputStream(html.getBytes(StandardCharsets.UTF_8)));
+			IDE.openEditor(getWorkbenchPage(), file);
+		 } catch (Exception e) {
+	            PluginLogger.logError(e);
+	     }
+
+	}
+	
+
+	private String generateRegulation(ProcessDefinition definition)
+			throws Exception {
+
+		Configuration config = new Configuration();
+
+		config.setObjectWrapper(ObjectWrapper.DEFAULT_WRAPPER);
+		config.setDefaultEncoding("UTF-8");
+		config.setTemplateExceptionHandler(TemplateExceptionHandler.HTML_DEBUG_HANDLER);
+
+		// TODO need localization
+		Path path = new Path("template/regulation.ftl");
+
+		Bundle bundl = Activator.getDefault().getBundle();
+		URL url = FileLocator.find(bundl, path, Collections.EMPTY_MAP);
+		URL fileUrl = FileLocator.toFileURL(url);
+		InputStream input = fileUrl.openConnection().getInputStream();
+		Template template = new Template("regulation", new StringReader(
+				IOUtils.readStream(input)), config);
+
+		HashMap<String, Object> map = new HashMap<String, Object>();
+		map.put("proc", definition);
+
+		IFile htmlDefinition = IOUtils.getAdjacentFile(
+				getActiveDesignerEditor().getDefinitionFile(),
+				ParContentProvider.PROCESS_DEFINITION_DESCRIPTION_FILE_NAME);
+		if (htmlDefinition.exists()) {
+			map.put("brief", IOUtils.readStream(htmlDefinition.getContents()));
+		}
+
+		// freeMarker can't work with params like StartState.class directly
+		HashMap<String, Object> model = new HashMap<String, Object>();
+		map.put("model", model);
+		model.put("start", StartState.class);
+		model.put("task", TaskState.class);
+		model.put("node", Node.class);
+		model.put("end", EndState.class);
+		model.put("variable", Variable.class);
+		model.put("timer", Timer.class);
+		model.put("endToken", EndTokenState.class);
+
+		Writer writer = new StringWriter();
+		template.process(map, writer);
+		return writer.toString();
+		
+	}
+
+}
