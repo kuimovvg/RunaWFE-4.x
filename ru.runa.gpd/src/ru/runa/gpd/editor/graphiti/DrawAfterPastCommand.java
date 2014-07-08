fb8c0859519f3c27f9b28115371b057ddf646df5
mihaj <mihaj@999c22fd-c5eb-e245-b5be-d41808c6d2cd>
2014-07-08 09:34:15 +0000

    -fix #699 bpmn, copy/paste, процесс не открывается после копирования/вставки ( https://sourceforge.net/p/runawfe/bugs/699/ )  -fix #698 bpmn, copy/paste, Paste operation failed ( https://sourceforge.net/p/runawfe/bugs/698/ )  -fix #700 Копирование элемента Роль(вертикальный/горизонтальный) в процесс который был создан с опцией "Показывать роли на графе"=НЕТ ( https://sourceforge.net/p/runawfe/bugs/700/ )
    git-svn-id: svn://alcomputer/RunaWFE-4.x/trunk/projects/gpd/plugins@5761 999c22fd-c5eb-e245-b5be-d41808c6d2cd

--- ru.runa.gpd/src/ru/runa/gpd/editor/graphiti/DrawAfterPastCommand.java
+++ ru.runa.gpd/src/ru/runa/gpd/editor/graphiti/DrawAfterPastCommand.java
@@ -8,7 +8,9 @@ import org.eclipse.gef.commands.Command;
 import ru.runa.gpd.lang.Language;
 import ru.runa.gpd.lang.model.NamedGraphElement;
 import ru.runa.gpd.lang.model.ProcessDefinition;
+import ru.runa.gpd.lang.model.Swimlane;
 import ru.runa.gpd.lang.model.Transition;
+import ru.runa.gpd.util.SwimlaneDisplayMode;
 
 /**
  * This is second part of Paste method. But EMF need to do it on other transaction/command
@@ -32,12 +34,25 @@ public class DrawAfterPastCommand extends Command {
 	
 	@Override
 	public void execute() {
+		if ( processDefinition.getSwimlaneDisplayMode() == SwimlaneDisplayMode.none) {
+			removeSwimlanes(elementList);
+		}
 		diagramPage.drawElements(diagramPage.getDiagramTypeProvider().getDiagram(), elementList);
 		List<Transition> transitions = new ArrayList<Transition>();
 		for (NamedGraphElement element : elementList) {
 			transitions.addAll(element.getChildrenRecursive(Transition.class));
 		}
 		diagramPage.drawTransitions(transitions);
+	}
+	
+	private void removeSwimlanes(List<NamedGraphElement> elementList ) {
+		List<NamedGraphElement> toRemove = new ArrayList<NamedGraphElement>();
+		for ( NamedGraphElement element : elementList) {
+			if ( element instanceof Swimlane ) {
+				toRemove.add(element);
+			}
+		}
+		elementList.removeAll(toRemove);
 	}
 	
 	@Override
