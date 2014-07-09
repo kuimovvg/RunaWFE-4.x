1d305b60ee934830cf0254d3df6e020c0f737367
mihaj <mihaj@999c22fd-c5eb-e245-b5be-d41808c6d2cd>
2014-07-09 08:04:19 +0000

    -fix  #700 Копирование элемента Роль(вертикальный/горизонтальный) в процесс который был создан с опцией "Показывать роли на графе"=НЕТ  ( https://sourceforge.net/p/runawfe/bugs/700/ )
    git-svn-id: svn://alcomputer/RunaWFE-4.x/trunk@5770 999c22fd-c5eb-e245-b5be-d41808c6d2cd

--- projects/gpd/plugins/ru.runa.gpd/src/ru/runa/gpd/editor/PasteAction.java
+++ projects/gpd/plugins/ru.runa.gpd/src/ru/runa/gpd/editor/PasteAction.java
@@ -4,7 +4,7 @@ import org.eclipse.core.resources.IFolder;
 import org.eclipse.gef.ui.actions.SelectionAction;
 
 import ru.runa.gpd.Localization;
-import ru.runa.gpd.editor.graphiti.DrawAfterPastCommand;
+import ru.runa.gpd.editor.graphiti.DrawAfterPasteCommand;
 
 public class PasteAction extends SelectionAction {
     private final ProcessEditorBase editor;
@@ -28,7 +28,7 @@ public class PasteAction extends SelectionAction {
     public void run() {
         CopyGraphCommand copy = createCommand();
         execute(copy);
-        execute(new DrawAfterPastCommand(copy.getFilteredElements(), editor.getDefinition(), editor.getDiagramEditorPage()));
+        execute(new DrawAfterPasteCommand(copy.getFilteredElements(), editor.getDefinition(), editor.getDiagramEditorPage()));
 
     }
 }
