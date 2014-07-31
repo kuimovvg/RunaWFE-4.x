28c1aefc78e304c161c68b4111d036e785cb827f
gritsenko_s <gritsenko_s@999c22fd-c5eb-e245-b5be-d41808c6d2cd>
2014-07-31 14:39:52 +0000

    fixed #738
    git-svn-id: svn://alcomputer/RunaWFE-4.x/trunk/projects/gpd/plugins/ru.runa.gpd@5820 999c22fd-c5eb-e245-b5be-d41808c6d2cd

--- src/ru/runa/gpd/ui/custom/VariableNameChecker.java
+++ src/ru/runa/gpd/ui/custom/VariableNameChecker.java
@@ -9,21 +9,21 @@ import com.google.common.collect.Lists;
 
 public class VariableNameChecker extends KeyAdapter {
     private static final List<Character> forbiddenCharacters = Lists.newArrayList('"', '\'', '&', '@', '>', '<', '\\', '.', '|');
-        
+
     @Override
     public void keyPressed(KeyEvent e) {
         if (forbiddenCharacters.contains(e.character)) {
             e.doit = false;
         }
     }
-    
+
     public static boolean isValid(String string) {
         for (char c : string.toCharArray()) {
             if (forbiddenCharacters.contains(c)) {
                 return false;
             }
         }
-        if (string.endsWith(" ")) {
+        if (string.startsWith(" ") || string.endsWith(" ")) {
             return false;
         }
         return true;
