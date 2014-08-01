2aaa0efa917198fb55d9fca19f4ae3de8b2f72de
gritsenko_s <gritsenko_s@999c22fd-c5eb-e245-b5be-d41808c6d2cd>
2014-08-01 16:40:14 +0000

    fixed #760, #761
    git-svn-id: svn://alcomputer/RunaWFE-4.x/branches/v4.1.2@5826 999c22fd-c5eb-e245-b5be-d41808c6d2cd

--- wfe/wfe-core/src/main/java/ru/runa/wfe/var/converter/FileVariableToByteArrayConverter.java
+++ wfe/wfe-core/src/main/java/ru/runa/wfe/var/converter/FileVariableToByteArrayConverter.java
@@ -1,14 +1,12 @@
 package ru.runa.wfe.var.converter;
 
-import java.io.File;
 import java.io.IOException;
-
-import org.springframework.beans.factory.InitializingBean;
 
 import ru.runa.wfe.InternalApplicationException;
 import ru.runa.wfe.commons.SystemProperties;
 import ru.runa.wfe.var.FileVariable;
 import ru.runa.wfe.var.FileVariableDescriptor;
+import ru.runa.wfe.var.FileVariableStorage;
 import ru.runa.wfe.var.Variable;
 
 import com.google.common.io.Files;
@@ -20,36 +18,12 @@ import com.google.common.io.Files;
  * @author dofs
  * @since 4.0
  */
-public class FileVariableToByteArrayConverter extends SerializableToByteArrayConverter implements InitializingBean {
+public class FileVariableToByteArrayConverter extends SerializableToByteArrayConverter {
     private static final long serialVersionUID = 1L;
-    private File storageDir = new File(SystemProperties.getLocalFileStoragePath());
-
-    @Override
-    public void afterPropertiesSet() throws Exception {
-        if (SystemProperties.isLocalFileStorageEnabled()) {
-            storageDir.mkdirs();
-        }
-    }
 
     @Override
     public boolean supports(Object value) {
         return value instanceof FileVariable;
-    }
-
-    private File getContentFile(FileVariableDescriptor descriptor, boolean create) {
-        File file = new File(storageDir, descriptor.getVariablePath());
-        if (create) {
-            file.getParentFile().mkdirs();
-            try {
-                file.createNewFile();
-            } catch (IOException e) {
-                throw new InternalApplicationException("Unable to create file '" + file + "'");
-            }
-        }
-        if (!file.exists()) {
-            throw new InternalApplicationException("No file found by path '" + file + "'");
-        }
-        return file;
     }
 
     @Override
@@ -58,24 +32,13 @@ public class FileVariableToByteArrayConverter extends SerializableToByteArrayCon
         if (SystemProperties.isLocalFileStorageEnabled() && fileVariable.getData().length > SystemProperties.getLocalFileStorageFileLimit()) {
             try {
                 FileVariableDescriptor descriptor = new FileVariableDescriptor(variable, fileVariable);
-                Files.write(descriptor.getData(), getContentFile(descriptor, true));
+                Files.write(descriptor.getData(), FileVariableStorage.getContentFile(descriptor, true));
                 return super.convert(variable, descriptor);
             } catch (IOException e) {
                 throw new InternalApplicationException("Unable to save file variable to local drive", e);
             }
         }
         return super.convert(variable, o);
-    }
-
-    @Override
-    public Object revert(Object o) {
-        FileVariable fileVariable = (FileVariable) super.revert(o);
-        if (fileVariable instanceof FileVariableDescriptor) {
-            FileVariableDescriptor descriptor = (FileVariableDescriptor) fileVariable;
-            descriptor.setFile(getContentFile(descriptor, false));
-            return descriptor;
-        }
-        return fileVariable;
     }
 
 }
