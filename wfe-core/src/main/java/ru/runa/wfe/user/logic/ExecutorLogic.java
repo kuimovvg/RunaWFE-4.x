84562641e7ff635a61a21c8db8b553c19531a617
gritsenko_s <gritsenko_s@999c22fd-c5eb-e245-b5be-d41808c6d2cd>
2014-08-18 15:46:43 +0000

    assignment fix
    git-svn-id: svn://alcomputer/RunaWFE-4.x/trunk/projects/wfe@5842 999c22fd-c5eb-e245-b5be-d41808c6d2cd

--- wfe-core/src/main/java/ru/runa/wfe/user/logic/ExecutorLogic.java
+++ wfe-core/src/main/java/ru/runa/wfe/user/logic/ExecutorLogic.java
@@ -46,7 +46,6 @@ import ru.runa.wfe.user.ExecutorParticipatesInProcessesException;
 import ru.runa.wfe.user.ExecutorPermission;
 import ru.runa.wfe.user.Group;
 import ru.runa.wfe.user.GroupPermission;
-import ru.runa.wfe.user.TemporaryGroup;
 import ru.runa.wfe.user.User;
 import ru.runa.wfe.user.dao.ProfileDAO;
 
@@ -148,11 +147,9 @@ public class ExecutorLogic extends CommonLogic {
         if (permissionDAO.isPrivilegedExecutor(executor)) {
             throw new AuthorizationException(executor.getName() + " can not be removed");
         }
-        if (!(executor instanceof TemporaryGroup)) {
-            Set<Number> processIds = processDAO.getDependentProcessIds(executor);
-            if (processIds.size() > 0) {
-                throw new ExecutorParticipatesInProcessesException(executor.getName(), processIds);
-            }
+        Set<Number> processIds = processDAO.getDependentProcessIds(executor);
+        if (processIds.size() > 0) {
+            throw new ExecutorParticipatesInProcessesException(executor.getName(), processIds);
         }
         if (executor instanceof Actor) {
             profileDAO.delete((Actor) executor);
