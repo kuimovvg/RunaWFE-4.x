803d7e770f70a1e13fc567df0d86ecd78933ff22
gritsenko_s <gritsenko_s@999c22fd-c5eb-e245-b5be-d41808c6d2cd>
2014-08-18 10:12:53 +0000

    BotInvokerService now exported as web service; javadoc
    git-svn-id: svn://alcomputer/RunaWFE-4.x/trunk@5840 999c22fd-c5eb-e245-b5be-d41808c6d2cd

--- projects/wfe/wfe-bots/src/main/java/ru/runa/wfe/service/BotInvokerService.java
+++ projects/wfe/wfe-bots/src/main/java/ru/runa/wfe/service/BotInvokerService.java
@@ -7,12 +7,30 @@ import ru.runa.wfe.bot.BotStation;
 @Remote
 public interface BotInvokerService {
 
+    /**
+     * Execute pending tasks in bot station once.
+     * 
+     * @param botStation
+     */
     public void invokeBots(BotStation botStation);
 
+    /**
+     * Start bot station (tasks are executed periodically).
+     * 
+     * @param botStation
+     */
     public void startPeriodicBotsInvocation(BotStation botStation);
 
+    /**
+     * Tests whether bot station is started.
+     * 
+     * @return
+     */
     public boolean isRunning();
 
+    /**
+     * Stop bot station (if it was started).
+     */
     public void cancelPeriodicBotsInvocation();
 
 }
