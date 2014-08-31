c2f48dadfc45fc8389e6642edb92fc5c3732f709
gritsenko_s <gritsenko_s@999c22fd-c5eb-e245-b5be-d41808c6d2cd>
2014-08-31 20:10:26 +0000

    new constructors allows to use proxied services
    git-svn-id: svn://alcomputer/RunaWFE-4.x/trunk/projects@5877 999c22fd-c5eb-e245-b5be-d41808c6d2cd

--- wfe/wfe-service/src/main/java/ru/runa/wfe/service/client/DelegateDefinitionVariableProvider.java
+++ wfe/wfe-service/src/main/java/ru/runa/wfe/service/client/DelegateDefinitionVariableProvider.java
@@ -3,6 +3,7 @@ package ru.runa.wfe.service.client;
 import java.util.List;
 
 import ru.runa.wfe.lang.SwimlaneDefinition;
+import ru.runa.wfe.service.DefinitionService;
 import ru.runa.wfe.service.delegate.Delegates;
 import ru.runa.wfe.user.User;
 import ru.runa.wfe.var.AbstractVariableProvider;
@@ -12,12 +13,18 @@ import ru.runa.wfe.var.dto.WfVariable;
 import com.google.common.base.Objects;
 
 public class DelegateDefinitionVariableProvider extends AbstractVariableProvider {
+    private final DefinitionService definitionService;
     private final User user;
     private final Long definitionId;
 
-    public DelegateDefinitionVariableProvider(User user, Long definitionId) {
+    public DelegateDefinitionVariableProvider(DefinitionService definitionService, User user, Long definitionId) {
+        this.definitionService = definitionService;
         this.user = user;
         this.definitionId = definitionId;
+    }
+
+    public DelegateDefinitionVariableProvider(User user, Long definitionId) {
+        this(Delegates.getDefinitionService(), user, definitionId);
     }
 
     @Override
@@ -27,7 +34,7 @@ public class DelegateDefinitionVariableProvider extends AbstractVariableProvider
 
     @Override
     public String getProcessDefinitionName() {
-        return Delegates.getDefinitionService().getProcessDefinition(user, definitionId).getName();
+        return definitionService.getProcessDefinition(user, definitionId).getName();
     }
 
     @Override
@@ -37,11 +44,11 @@ public class DelegateDefinitionVariableProvider extends AbstractVariableProvider
 
     @Override
     public WfVariable getVariable(String variableName) {
-        VariableDefinition variableDefinition = Delegates.getDefinitionService().getVariableDefinition(user, definitionId, variableName);
+        VariableDefinition variableDefinition = definitionService.getVariableDefinition(user, definitionId, variableName);
         if (variableDefinition != null) {
             return new WfVariable(variableDefinition, null);
         }
-        List<SwimlaneDefinition> swimlaneDefinitions = Delegates.getDefinitionService().getSwimlaneDefinitions(user, definitionId);
+        List<SwimlaneDefinition> swimlaneDefinitions = definitionService.getSwimlaneDefinitions(user, definitionId);
         for (SwimlaneDefinition swimlaneDefinition : swimlaneDefinitions) {
             if (Objects.equal(variableName, swimlaneDefinition.getName())) {
                 return new WfVariable(swimlaneDefinition.toVariableDefinition(), null);
