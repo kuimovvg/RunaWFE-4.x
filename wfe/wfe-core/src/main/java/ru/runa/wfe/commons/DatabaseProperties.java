f804cd4615996fdd28bf8f84a3ed6e345a73b747
petrmikheev <petrmikheev@999c22fd-c5eb-e245-b5be-d41808c6d2cd>
2014-08-31 18:39:37 +0000

    Error checking for commit #5867
    git-svn-id: svn://alcomputer/RunaWFE-4.x/trunk/projects@5871 999c22fd-c5eb-e245-b5be-d41808c6d2cd

--- wfe/wfe-core/src/main/java/ru/runa/wfe/commons/DatabaseProperties.java
+++ wfe/wfe-core/src/main/java/ru/runa/wfe/commons/DatabaseProperties.java
@@ -1,7 +1,7 @@
 package ru.runa.wfe.commons;
 
 public class DatabaseProperties {
-    private static final PropertyResources RESOURCES = new PropertyResources("database.properties", false);
+    private static final PropertyResources RESOURCES = new PropertyResources("database.properties", true, false);
 
     public static String getUserTransactionJndiName() {
         return RESOURCES.getStringPropertyNotNull("user.transaction.jndi.name");
