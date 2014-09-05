80622d4ccb70f34fb49eafc1576d0ea41dcd952f
mexanix <mexanix@999c22fd-c5eb-e245-b5be-d41808c6d2cd>
2014-09-05 11:22:25 +0000

    clean code
    git-svn-id: svn://alcomputer/RunaWFE-4.x/trunk/projects@5897 999c22fd-c5eb-e245-b5be-d41808c6d2cd

--- wfe/wfe-archiving/src/main/java/ru/runa/wfe/service/exceptions/PermissionDeniedException.java
+++ wfe/wfe-archiving/src/main/java/ru/runa/wfe/service/exceptions/PermissionDeniedException.java
@@ -2,7 +2,7 @@ package ru.runa.wfe.service.exceptions;
 
 public class PermissionDeniedException extends RuntimeException {
 
-    private static final String MESSAGE = "Недостаточно прав для выполнения операции";
+    private static final String MESSAGE = "Denied permissions to perform the operation";
     /**
 	 * 
 	 */
