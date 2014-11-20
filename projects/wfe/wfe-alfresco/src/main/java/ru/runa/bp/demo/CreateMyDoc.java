5a6aebbd28b91feee57f6e27924f72f2cf783a16
gritsenko_s <gritsenko_s@999c22fd-c5eb-e245-b5be-d41808c6d2cd>
2014-11-20 17:38:57 +0000

    IFileVariable issues
    git-svn-id: svn://alcomputer/RunaWFE-4.x/trunk@6010 999c22fd-c5eb-e245-b5be-d41808c6d2cd
    
    Former-commit-id: 05997355dbc3967dca10cb983ed1d555328a1e3d

--- projects/wfe/wfe-alfresco/src/main/java/ru/runa/bp/demo/CreateMyDoc.java
+++ projects/wfe/wfe-alfresco/src/main/java/ru/runa/bp/demo/CreateMyDoc.java
@@ -3,7 +3,7 @@ package ru.runa.bp.demo;
 import ru.runa.alfresco.AlfConnection;
 import ru.runa.bp.AlfHandler;
 import ru.runa.bp.AlfHandlerData;
-import ru.runa.wfe.var.FileVariable;
+import ru.runa.wfe.var.file.IFileVariable;
 
 public class CreateMyDoc extends AlfHandler {
 
@@ -11,7 +11,7 @@ public class CreateMyDoc extends AlfHandler {
     protected void executeAction(AlfConnection alfConnection, AlfHandlerData alfHandlerData) throws Exception {
         MyDoc myDoc = new MyDoc();
         alfConnection.createObject(myDoc);
-        FileVariable var = alfHandlerData.getInputParamValueNotNull(FileVariable.class, "file");
+        IFileVariable var = alfHandlerData.getInputParamValueNotNull(IFileVariable.class, "file");
         alfConnection.setContent(myDoc, var.getData(), var.getContentType());
         alfHandlerData.setOutputParam("uuid", myDoc.getUuidRef());
     }
