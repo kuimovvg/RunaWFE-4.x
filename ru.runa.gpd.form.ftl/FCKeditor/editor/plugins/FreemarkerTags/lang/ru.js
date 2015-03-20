944ba9cfea30eddc0146a68fc3a994b3143a4abb
gritsenko_s <gritsenko_s@999c22fd-c5eb-e245-b5be-d41808c6d2cd>
2015-03-20 16:35:08 +0000

    fixed #959
    git-svn-id: svn://alcomputer/RunaWFE-4.x/trunk/projects/gpd/plugins@6142 999c22fd-c5eb-e245-b5be-d41808c6d2cd

--- ru.runa.gpd.form.ftl/FCKeditor/editor/plugins/FreemarkerTags/lang/ru.js
+++ ru.runa.gpd.form.ftl/FCKeditor/editor/plugins/FreemarkerTags/lang/ru.js
@@ -1,10 +1,10 @@
 
-FCKLang.MethodTitle = 'Freemarker тег';
+FCKLang.MethodTitle = 'Параметры компонента';
 FCKLang.OutputTitle = 'Вывод переменной';
 FCKLang.FtlVariable = 'Переменная';
 FCKLang.FtlMethodParameters = 'Параметры';
 FCKLang.FtlFormat = 'Формат переменной';
-FCKLang.FtlMethod = 'Тег';
+FCKLang.FtlMethod = 'Компонент';
 FCKLang.ErrNoVar = 'Выберите переменную';
 FCKLang.ErrNoTagFormat = 'Выберите формат';
 FCKLang.ErrNoTagType = 'Выберите тег';
