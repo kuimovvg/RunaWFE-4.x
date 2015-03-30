ac1baeedd74fac5f87a1798e1917bfa5a5f3b462
gritsenko_s <gritsenko_s@999c22fd-c5eb-e245-b5be-d41808c6d2cd>
2015-03-30 03:53:17 +0000

    added ws method to get variable by name
    git-svn-id: svn://alcomputer/RunaWFE-4.x/trunk/projects/wfe@6163 999c22fd-c5eb-e245-b5be-d41808c6d2cd

--- wfe-service/src/main/java/ru/runa/wfe/service/impl/ExecutionServiceBean.java
+++ wfe-service/src/main/java/ru/runa/wfe/service/impl/ExecutionServiceBean.java
@@ -1,18 +1,18 @@
 /*
  * This file is part of the RUNA WFE project.
- * 
- * This program is free software; you can redistribute it and/or 
- * modify it under the terms of the GNU Lesser General Public License 
- * as published by the Free Software Foundation; version 2.1 
- * of the License. 
- * 
- * This program is distributed in the hope that it will be useful, 
- * but WITHOUT ANY WARRANTY; without even the implied warranty of 
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the 
- * GNU Lesser General Public License for more details. 
- * 
- * You should have received a copy of the GNU Lesser General Public License 
- * along with this program; if not, write to the Free Software 
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; version 2.1
+ * of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA.
  */
 package ru.runa.wfe.service.impl;
@@ -307,6 +307,17 @@ public class ExecutionServiceBean implements ExecutionServiceLocal, ExecutionSer
         Preconditions.checkArgument(user != null);
         Preconditions.checkArgument(processId != null);
         executionLogic.upgradeProcessToNextDefinitionVersion(user, processId);
+    }
+
+    @Override
+    @WebResult(name = "result")
+    public Variable getVariableWS(@WebParam(name = "user") User user, @WebParam(name = "processId") Long processId,
+            @WebParam(name = "variableName") String variableName) {
+        WfVariable variable = getVariable(user, processId, variableName);
+        if (variable != null) {
+            return VariableConverter.marshal(variable.getDefinition(), variable.getValue());
+        }
+        return null;
     }
 
     @Override
