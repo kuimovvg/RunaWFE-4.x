b0d5cade2da0c48b49a49975fc6c49cd1490287c
gritsenko_s <gritsenko_s@999c22fd-c5eb-e245-b5be-d41808c6d2cd>
2015-04-03 10:34:01 +0000

    fixed: minimazed view tooltips drawing, appropriate nodeId in process logs
    git-svn-id: svn://alcomputer/RunaWFE-4.x/trunk/projects/wfe@6183 999c22fd-c5eb-e245-b5be-d41808c6d2cd

--- wfe-core/src/main/java/ru/runa/wfe/graph/image/figure/bpmn/RoundedRect.java
+++ wfe-core/src/main/java/ru/runa/wfe/graph/image/figure/bpmn/RoundedRect.java
@@ -1,18 +1,18 @@
 /*
  * This file is part of the RUNA WFE project.
- * 
- * This program is free software; you can redistribute it and/or 
- * modify it under the terms of the GNU Lesser General Public License 
- * as published by the Free Software Foundation; version 2.1 
- * of the License. 
- * 
- * This program is distributed in the hope that it will be useful, 
- * but WITHOUT ANY WARRANTY; without even the implied warranty of 
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the 
- * GNU Lesser General Public License for more details. 
- * 
- * You should have received a copy of the GNU Lesser General Public License 
- * along with this program; if not, write to the Free Software 
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; version 2.1
+ * of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA.
  */
 package ru.runa.wfe.graph.image.figure.bpmn;
@@ -58,14 +58,6 @@ public class RoundedRect extends AbstractFigure {
             r.grow(-DrawProperties.GRID_SIZE / 2, -DrawProperties.GRID_SIZE / 2);
         }
         return r;
-    }
-
-    @Override
-    public Rectangle getRectangle() {
-        if (minimized) {
-            return new Rectangle(coords[0], coords[1], 3 * DrawProperties.GRID_SIZE, 3 * DrawProperties.GRID_SIZE);
-        }
-        return super.getRectangle();
     }
 
 }
