53fc3331906af605a26d435d093b01812ac08cbf
gritsenko_s <gritsenko_s@999c22fd-c5eb-e245-b5be-d41808c6d2cd>
2015-04-10 04:13:43 +0000

    synchronization removed due to fails in sequenced bot tasks
    git-svn-id: svn://alcomputer/RunaWFE-4.x/trunk/projects@6202 999c22fd-c5eb-e245-b5be-d41808c6d2cd

--- wfe/wfe-bots/src/main/java/ru/runa/wfe/service/handler/BotInvokerActionHandler.java
+++ wfe/wfe-bots/src/main/java/ru/runa/wfe/service/handler/BotInvokerActionHandler.java
@@ -1,25 +1,23 @@
 /*
  * This file is part of the RUNA WFE project.
- * 
- * This program is free software; you can redistribute it and/or 
- * modify it under the terms of the GNU Lesser General Public License 
- * as published by the Free Software Foundation; version 2.1 
- * of the License. 
- * 
- * This program is distributed in the hope that it will be useful, 
- * but WITHOUT ANY WARRANTY; without even the implied warranty of 
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the 
- * GNU Lesser General Public License for more details. 
- * 
- * You should have received a copy of the GNU Lesser General Public License 
- * along with this program; if not, write to the Free Software 
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; version 2.1
+ * of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA.
  */
 package ru.runa.wfe.service.handler;
 
 import java.util.List;
-import java.util.Map;
-import java.util.concurrent.Future;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.ScheduledThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
@@ -32,16 +30,14 @@ import ru.runa.wfe.service.delegate.BotInvokerServiceDelegate;
 import ru.runa.wfe.service.delegate.Delegates;
 
 import com.google.common.base.Strings;
-import com.google.common.collect.Maps;
 
 /**
  * Starts bot invocation at specified server.
- * 
+ *
  * @since 2.0
  */
 public class BotInvokerActionHandler extends ActionHandlerBase {
     private static final ScheduledExecutorService scheduledExecutorService = new ScheduledThreadPoolExecutor(1);
-    private static final Map<BotStation, Future<?>> invocations = Maps.newHashMap();
 
     @Override
     public void execute(ExecutionContext executionContext) {
@@ -68,17 +64,9 @@ public class BotInvokerActionHandler extends ActionHandlerBase {
                 log.warn("No botstation can be found for invocation " + configuration);
                 return;
             }
-            synchronized (botStation) {
-                Future<?> invocation = invocations.get(botStation);
-                if (invocation != null && !invocation.isDone()) {
-                    log.debug("invocation != null && !invocation.isDone() for " + botStation);
-                    return;
-                }
-                long timeout = BotStationResources.getBotInvokerHandlerTimeout();
-                log.debug("Scheduling invocation of " + botStation + " for " + timeout + "ms");
-                invocation = scheduledExecutorService.schedule(new InvokerRunnable(botStation), timeout, TimeUnit.MILLISECONDS);
-                invocations.put(botStation, invocation);
-            }
+            long timeout = BotStationResources.getBotInvokerHandlerTimeout();
+            log.debug("Scheduling invocation of " + botStation + " for " + timeout + "ms");
+            scheduledExecutorService.schedule(new InvokerRunnable(botStation), timeout, TimeUnit.MILLISECONDS);
         } catch (Exception e) {
             log.error("Unable to invoke bot station due to " + e);
         }
