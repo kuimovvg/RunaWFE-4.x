bdcddaccae71bedfd70632fc9c7be12b2cfae449
kanaal <kanaal@999c22fd-c5eb-e245-b5be-d41808c6d2cd>
2015-04-18 10:52:50 +0000

    Changing Types.BOOLEAN to Types.BIT
    git-svn-id: svn://alcomputer/RunaWFE-4.x/trunk/projects/wfe@6226 999c22fd-c5eb-e245-b5be-d41808c6d2cd

--- wfe-core/src/main/java/ru/runa/wfe/commons/dbpatch/impl/AddSequentialFlagToBot.java
+++ wfe-core/src/main/java/ru/runa/wfe/commons/dbpatch/impl/AddSequentialFlagToBot.java
@@ -13,8 +13,8 @@ public class AddSequentialFlagToBot extends DBPatch {
     @Override
     protected List<String> getDDLQueriesBefore() {
         List<String> sql = super.getDDLQueriesBefore();
-        sql.add(getDDLCreateColumn("BOT_TASK", new ColumnDef("IS_SEQUENTIAL", dialect.getTypeName(Types.BOOLEAN))));
-        sql.add(getDDLCreateColumn("BOT", new ColumnDef("IS_SEQUENTIAL", dialect.getTypeName(Types.BOOLEAN))));
+        sql.add(getDDLCreateColumn("BOT_TASK", new ColumnDef("IS_SEQUENTIAL", dialect.getTypeName(Types.BIT))));
+        sql.add(getDDLCreateColumn("BOT", new ColumnDef("IS_SEQUENTIAL", dialect.getTypeName(Types.BIT))));
         return sql;
     }
 
