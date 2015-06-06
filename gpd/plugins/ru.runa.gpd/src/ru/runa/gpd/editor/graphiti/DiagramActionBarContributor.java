b2c1eb702f2a2a7b8b06e38bb87a4c2e2c99dca4
flyninja <flyninja@999c22fd-c5eb-e245-b5be-d41808c6d2cd>
2015-06-06 20:32:29 +0000

    #985 - полный мерж всего наработанного по тикету
    git-svn-id: svn://alcomputer/RunaWFE-4.x/trunk/projects@6338 999c22fd-c5eb-e245-b5be-d41808c6d2cd

--- gpd/plugins/ru.runa.gpd/src/ru/runa/gpd/editor/graphiti/DiagramActionBarContributor.java
+++ gpd/plugins/ru.runa.gpd/src/ru/runa/gpd/editor/graphiti/DiagramActionBarContributor.java
@@ -10,7 +10,6 @@ import org.eclipse.graphiti.ui.services.GraphitiUi;
 import org.eclipse.jface.action.IAction;
 import org.eclipse.jface.action.IToolBarManager;
 import org.eclipse.jface.action.Separator;
-import org.eclipse.ui.actions.ActionFactory;
 import org.eclipse.ui.actions.RetargetAction;
 
 import ru.runa.gpd.editor.ProcessEditorContributor;
@@ -26,24 +25,37 @@ public class DiagramActionBarContributor extends ProcessEditorContributor {
     @Override
     protected void buildActions() {
         super.buildActions();
-        
-//        addRetargetAction(new AlignmentRetargetAction(PositionConstants.LEFT));
-//        addRetargetAction(new AlignmentRetargetAction(PositionConstants.CENTER));
-//        addRetargetAction(new AlignmentRetargetAction(PositionConstants.RIGHT));
-//        addRetargetAction(new AlignmentRetargetAction(PositionConstants.TOP));
-//        addRetargetAction(new AlignmentRetargetAction(PositionConstants.MIDDLE));
-//        addRetargetAction(new AlignmentRetargetAction(PositionConstants.BOTTOM));
-//        addRetargetAction(new MatchWidthRetargetAction());
-//        addRetargetAction(new MatchHeightRetargetAction());
+
+        // addRetargetAction(new
+        // AlignmentRetargetAction(PositionConstants.LEFT));
+        // addRetargetAction(new
+        // AlignmentRetargetAction(PositionConstants.CENTER));
+        // addRetargetAction(new
+        // AlignmentRetargetAction(PositionConstants.RIGHT));
+        // addRetargetAction(new
+        // AlignmentRetargetAction(PositionConstants.TOP));
+        // addRetargetAction(new
+        // AlignmentRetargetAction(PositionConstants.MIDDLE));
+        // addRetargetAction(new
+        // AlignmentRetargetAction(PositionConstants.BOTTOM));
+        // addRetargetAction(new MatchWidthRetargetAction());
+        // addRetargetAction(new MatchHeightRetargetAction());
 
         addRetargetAction(new ZoomInRetargetAction());
         addRetargetAction(new ZoomOutRetargetAction());
-        //        addRetargetAction(new RetargetAction(GEFActionConstants.TOGGLE_GRID_VISIBILITY, Messages.DiagramEditorActionBarContributor_Grid, IAction.AS_CHECK_BOX));
-        //        addRetargetAction(new RetargetAction(GEFActionConstants.TOGGLE_SNAP_TO_GEOMETRY, Messages.DiagramEditorActionBarContributor_SnapGeometry, IAction.AS_CHECK_BOX));
+        // addRetargetAction(new
+        // RetargetAction(GEFActionConstants.TOGGLE_GRID_VISIBILITY,
+        // Messages.DiagramEditorActionBarContributor_Grid,
+        // IAction.AS_CHECK_BOX));
+        // addRetargetAction(new
+        // RetargetAction(GEFActionConstants.TOGGLE_SNAP_TO_GEOMETRY,
+        // Messages.DiagramEditorActionBarContributor_SnapGeometry,
+        // IAction.AS_CHECK_BOX));
 
         // Bug 323351: Add button to toggle a flag if the context pad buttons
         // shall be shown or not
-        RetargetAction toggleContextPadAction = new RetargetAction(ToggleContextButtonPadAction.ACTION_ID, ToggleContextButtonPadAction.TEXT, IAction.AS_CHECK_BOX);
+        RetargetAction toggleContextPadAction = new RetargetAction(ToggleContextButtonPadAction.ACTION_ID, ToggleContextButtonPadAction.TEXT,
+                IAction.AS_CHECK_BOX);
         toggleContextPadAction.setImageDescriptor(GraphitiUi.getImageService().getImageDescriptorForId(IPlatformImageConstants.IMG_TOGGLE_PAD));
         addRetargetAction(toggleContextPadAction);
         // End bug 323351
@@ -60,19 +72,17 @@ public class DiagramActionBarContributor extends ProcessEditorContributor {
      */
     @Override
     public void contributeToToolBar(IToolBarManager tbm) {
-        super.contributeToToolBar(tbm);
-        
-//        tbm.add(new Separator());
-//        tbm.add(getAction(GEFActionConstants.ALIGN_LEFT));
-//        tbm.add(getAction(GEFActionConstants.ALIGN_CENTER));
-//        tbm.add(getAction(GEFActionConstants.ALIGN_RIGHT));
-//        tbm.add(new Separator());
-//        tbm.add(getAction(GEFActionConstants.ALIGN_TOP));
-//        tbm.add(getAction(GEFActionConstants.ALIGN_MIDDLE));
-//        tbm.add(getAction(GEFActionConstants.ALIGN_BOTTOM));
-//        tbm.add(new Separator());
-//        tbm.add(getAction(GEFActionConstants.MATCH_WIDTH));
-//        tbm.add(getAction(GEFActionConstants.MATCH_HEIGHT));
+        // tbm.add(new Separator());
+        // tbm.add(getAction(GEFActionConstants.ALIGN_LEFT));
+        // tbm.add(getAction(GEFActionConstants.ALIGN_CENTER));
+        // tbm.add(getAction(GEFActionConstants.ALIGN_RIGHT));
+        // tbm.add(new Separator());
+        // tbm.add(getAction(GEFActionConstants.ALIGN_TOP));
+        // tbm.add(getAction(GEFActionConstants.ALIGN_MIDDLE));
+        // tbm.add(getAction(GEFActionConstants.ALIGN_BOTTOM));
+        // tbm.add(new Separator());
+        // tbm.add(getAction(GEFActionConstants.MATCH_WIDTH));
+        // tbm.add(getAction(GEFActionConstants.MATCH_HEIGHT));
 
         // Bug 323351: Add button to toggle a flag if the context pad buttons
         // shall be shown or not
@@ -85,10 +95,6 @@ public class DiagramActionBarContributor extends ProcessEditorContributor {
         tbm.add(getAction(GEFActionConstants.ZOOM_IN));
         ZoomComboContributionItem zoomCombo = new ZoomComboContributionItem(getPage());
         tbm.add(zoomCombo);
-        
-        tbm.add(getAction(ActionFactory.COPY.getId()));
-        tbm.add(getAction(ActionFactory.PASTE.getId()));
-
-        tbm.add(new Separator());
+        super.contributeToToolBar(tbm);
     }
 }
