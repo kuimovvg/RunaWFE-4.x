e5f7ba7a7b339ba6f0e33d7546147a9fa107047a
gritsenko_s <gritsenko_s@999c22fd-c5eb-e245-b5be-d41808c6d2cd>
2015-06-09 05:27:47 +0000

    added http://runawfe.org/rus/doc/Handlers#SendEmailFtlSyntax
    git-svn-id: svn://alcomputer/RunaWFE-4.x/trunk@6345 999c22fd-c5eb-e245-b5be-d41808c6d2cd
    
    Former-commit-id: 35585a936f62fe14823cbdc6a098155c9d2fa6e7

--- projects/wfe/wfe-core/src/main/java/ru/runa/wfe/var/ScriptingVariableProvider.java
+++ projects/wfe/wfe-core/src/main/java/ru/runa/wfe/var/ScriptingVariableProvider.java
@@ -1,28 +1,15 @@
 package ru.runa.wfe.var;
 
-import ru.runa.wfe.lang.ProcessDefinition;
-
-import com.google.common.base.Objects;
 
 public class ScriptingVariableProvider extends DelegableVariableProvider {
-    private final ProcessDefinition processDefinition;
 
-    public ScriptingVariableProvider(ProcessDefinition processDefinition, IVariableProvider variableProvider) {
+    public ScriptingVariableProvider(IVariableProvider variableProvider) {
         super(variableProvider);
-        this.processDefinition = processDefinition;
     }
 
     @Override
     public Object getValue(String variableName) {
         Object object = super.getValue(variableName);
-        if (object == null) {
-            for (VariableDefinition definition : processDefinition.getVariables()) {
-                if (Objects.equal(variableName, definition.getScriptingName())) {
-                    object = super.getValue(definition.getName());
-                    break;
-                }
-            }
-        }
         if (object instanceof ComplexVariable) {
             object = new ScriptingComplexVariable((ComplexVariable) object);
         }
