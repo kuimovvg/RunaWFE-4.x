065a1532b612bfbbb1254b12eb5eac917ccafa7f
kanaal <kanaal@999c22fd-c5eb-e245-b5be-d41808c6d2cd>
2015-06-11 19:01:03 +0000

    Changed auto assembly
    git-svn-id: svn://alcomputer/RunaWFE-4.x/trunk@6356 999c22fd-c5eb-e245-b5be-d41808c6d2cd
    
    Former-commit-id: 80253760d1e8e94a194d63525b2521273865b7a7

--- auto_assembly/autorun.bat
+++ auto_assembly/autorun.bat
@@ -3,12 +3,16 @@ echo Version must be specified!!!
 exit
 )
 
+if "%2"=="" (
+echo Arch must be specified!!!
+exit
+)
+
 rd /S /Q build
 mkdir build
 
 copy jboss7.zip build
 copy eclipse-3.7.2-with-deltapack.zip build
-copy run.bat build
 copy createIso.bat build
 copy readme build
 
@@ -18,13 +22,19 @@ REM unrar x -tsm -tsc -tsa trunk.rar
 cd /D build
 jar xf eclipse-3.7.2-with-deltapack.zip
 del eclipse-3.7.2-with-deltapack.zip
+
 mkdir trunk
-cd trunk
-svn co https://svn.code.sf.net/p/runawfe/code/RunaWFE-4.x/trunk/projects
-cd ..
+svn export https://svn.code.sf.net/p/runawfe/code/RunaWFE-4.x/trunk/projects trunk/projects
+rem svn export svn://alcomputer/RunaWFE-4.x/trunk/projects
+
 mkdir trunk\docs
 mkdir trunk\docs\guides
 copy readme trunk\docs\guides\
+
+if "%2"=="64" (
+    del trunk\projects\installer\windows\resources\jdk-7u17-windows-i586.exe 
+    copy ..\jdk\jdk-7u7-windows-x64.exe trunk\projects\installer\windows\resources\jdk-7u17-windows-i586.exe 
+)
 
 cd trunk\projects\installer\windows\
 call mvn versions:set -DnewVersion=%1
@@ -38,9 +48,11 @@ cd ../wfe-app
 call mvn versions:set -DnewVersion=%1
 cd ../../rtn
 call mvn versions:set -DnewVersion=%1
+cd ../../wfe/wfe-cactus-it
+call mvn versions:set -DnewVersion=%1
 
 cd ..\installer\windows\
-copy ..\..\..\..\run.bat .
 copy ..\..\..\..\createIso.bat .
 
-call run.bat
+mvn clean package -Djboss.zip.file=../../../../jboss7.zip -Djboss.zip.folder=jboss7 -Declipse.home.dir=../../../../eclipse -Dappserver=jboss7
+call createIso.bat
--- auto_assembly/run.bat
+++
@@ -1,1 +0,0 @@
-mvn clean package -Djboss.zip.file=../../../../jboss7.zip -Djboss.zip.folder=jboss7 -Declipse.home.dir=../../../../eclipse -Dappserver=jboss7
