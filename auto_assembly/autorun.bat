89c7963bf7d9af04d2cec36e8b570ff550ca4b60
kanaal <kanaal@999c22fd-c5eb-e245-b5be-d41808c6d2cd>
2015-06-12 08:29:27 +0000

    auto build
    git-svn-id: svn://alcomputer/RunaWFE-4.x/trunk@6369 999c22fd-c5eb-e245-b5be-d41808c6d2cd
    
    Former-commit-id: f7c458f686e89eb97f7bb7e3ba89049057ca1b36

--- auto_assembly/autorun.bat
+++ auto_assembly/autorun.bat
@@ -57,4 +57,5 @@ cd ..\installer\windows\
 call mvn clean package -Djboss.zip.file=../../../../jboss7.zip -Djboss.zip.folder=jboss7 -Declipse.home.dir=../../../../eclipse -Dappserver=jboss7 -Djdk.dir="%~dp0%jdk"
 
 xcopy /E /Q target\test-result ..\..\..\..\..\results\test-result\
-xcopy /E /Q target\artifacts\Installer ..\..\..\..\..\results\Execution\
+xcopy /E /Q target\artifacts\Installer\RunaWFE-Installer.exe ..\..\..\..\..\results\Execution\
+xcopy /E /Q target\installer.iso ..\..\..\..\..\results\ISO\
